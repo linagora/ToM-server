45019eaf94a330a85b42e272ab7a8ba6a27b0c25
Yadd <yadd@debian.org>
2023-03-07 15:47:19 +0400

    API skeleton

--- packages/matrix-identity-server/README.md
+++ packages/matrix-identity-server/README.md
@@ -3,6 +3,28 @@
 Node.js library that implements
 [Matrix Identity Service API](https://spec.matrix.org/v1.6/identity-service-api/).
 
+## Synopsis
+
+Example using [express](https://www.npmjs.com/package/express):
+
+```js
+import express from 'express'
+import IdServer from '@twake/matrix-identity-server'
+const idServer = new IdServer()
+
+const app = express()
+
+Object.keys(idServer.api.get).forEach( k => {
+  app.get(k, idServer.api.get[k])
+})
+
+Object.keys(idServer.api.post).forEach( k => {
+  app.post(k, idServer.api.get[k])
+})
+
+app.listen(3000)
+```
+
 ## Copyright and license
 
 Copyright (c) 2023-present Linagora <https://linagora.com>
--- packages/matrix-identity-server/src/index.ts
+++ packages/matrix-identity-server/src/index.ts
@@ -1,3 +1,46 @@
-export class MatrixServer {
+import type http from 'http'
+import { Request, Response, NextFunction } from 'express'
 
+type expressAppHandler = (
+  req: Request | http.IncomingMessage,
+  res: Response | http.ServerResponse,
+  next?: NextFunction
+) => void;
+
+type IdServerAPI = {
+  [url: string]: expressAppHandler
+}
+
+export default class MatrixServer {
+  api: {
+    get: IdServerAPI,
+    post: IdServerAPI,
+    put?: IdServerAPI,
+  };
+
+  constructor () {
+    // TODO: insert here all endpoints
+    this.api = {
+      get: {
+        '/': (req,res) => {
+          this.send(res, 200, {"todo":"response"})
+        }
+      },
+      post: {
+        '/': (req,res) => {
+          this.send(res, 200, {"todo":"response"})
+        }
+      },
+    }
+  }
+
+  send(res: Response | http.ServerResponse, status: number, body: object): void {
+    const content = JSON.stringify(body)
+    res.writeHead(status, {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Content-Length': content.length,
+    });
+    res.write(content)
+    res.end()
+  }
 }
