a48b9f1f8cef7a7ea43873c74bc72ce1921651a0
Yadd <yadd@debian.org>
2023-03-07 17:53:02 +0400

    Coverage: 90%

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 export default {
   testEnvironment: "node",
   preset: 'ts-jest',
+  "collectCoverage": true,
+  "collectCoverageFrom": ["./src/**"],
+  "coverageThreshold": {
+    "global": {
+      "branches": 90,
+      "functions": 90,
+      "lines": 90,
+      "statements": 90,
+    }
+  },
 };
