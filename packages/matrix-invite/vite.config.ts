1769e4ccd7107a0d3cdd94825925038120b6ef5a
Khaled FERJANI <kferjani@linagora.com>
2023-04-12 09:48:33 +0100

    Expose a dynamic port for the invitation application

--- packages/matrix-invite/vite.config.ts
+++ packages/matrix-invite/vite.config.ts
@@ -1,9 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { loadEnv } from 'vite';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+export default defineConfig(({ mode }) => {
+	const env = { ...process.env, ...loadEnv(mode, process.cwd(), '') }
+
+	return {
+		plugins: [sveltekit()],
+		test: {
+			include: ['src/**/*.{test,spec}.{js,ts}']
+		},
+		server: {
+			port: parseInt(env.PORT || '3000')
+		}
+	};
 });
