0a322cb7bc2b67a695448968584beac865f40a0e
Yadd <yadd@debian.org>
2023-04-13 18:40:47 +0400

    Tom Id Server: minimal test

---
+++ packages/tom-identity-server/src/__testData__/buildUserDB.ts
@@ -0,0 +1,59 @@
+/* istanbul ignore file */
+import sqlite3 from 'sqlite3'
+import Pg from 'pg'
+
+interface Config {
+  database_host: string
+  [k: string]: any
+}
+
+let created = false
+
+const createQuery = 'CREATE TABLE users (uid varchar(8), phone varchar(12), email varchar(32))'
+const insertQuery = "INSERT INTO users VALUES('dwho', '33612345678', 'dwho@company.com')"
+
+// eslint-disable-next-line @typescript-eslint/promise-function-async
+const buildUserDB = (conf: Config): Promise<void> => {
+  if (created) return Promise.resolve()
+  return new Promise((resolve, reject) => {
+    if (conf.database_engine === 'sqlite') {
+      const userDb = new sqlite3.Database(conf.database_host)
+      userDb.run(createQuery, (err) => {
+        if (err != null) {
+          reject(err)
+        } else {
+          userDb.run(insertQuery, (err) => {
+            if (err != null) {
+              reject(err)
+            } else {
+              userDb.close()
+              created = true
+              resolve()
+            }
+          })
+        }
+      })
+    } else {
+      const userDb = new Pg.Client({
+        host: conf.database_host,
+        user: conf.database_user,
+        password: conf.database_password,
+        database: conf.database_name
+      })
+      userDb.connect().then(() => {
+        console.error('CONNECT')
+        userDb.query(createQuery).then(() => {
+          userDb.query(insertQuery).then(() => {
+            resolve()
+          }).catch(e => {
+            reject(e)
+          })
+        }).catch(e => {
+          reject(e)
+        })
+      }).catch(e => { reject(e) })
+    }
+  })
+}
+
+export default buildUserDB
---
+++ packages/tom-identity-server/src/index.test.ts
@@ -0,0 +1,158 @@
+import express from 'express'
+import request from 'supertest'
+import IdServer, { type Config } from './index'
+import fs from 'fs'
+import querystring from 'querystring'
+import fetch from 'node-fetch'
+import { Hash, supportedHashes } from '@twake/crypto'
+import defaultConfig from './__testData__/registerConf.json'
+import buildUserDB from './__testData__/buildUserDB'
+
+jest.mock('node-fetch', () => jest.fn())
+const sendMailMock = jest.fn()
+jest.mock('nodemailer', () => ({
+  createTransport: jest.fn().mockImplementation(() => ({
+    sendMail: sendMailMock
+  }))
+}))
+
+process.env.TWAKE_IDENTITY_SERVER_CONF = './src/__testData__/registerConf.json'
+
+let idServer: IdServer
+let app: express.Application
+let validToken: string
+
+beforeAll((done) => {
+  const conf: Config = {
+    ...defaultConfig,
+    database_engine: 'sqlite',
+    base_url: 'http://example.com/',
+    userdb_engine: 'sqlite'
+  }
+  if (process.env.TEST_PG === 'yes') {
+    conf.database_engine = 'pg'
+    conf.userdb_engine = 'pg'
+    conf.database_host = process.env.PG_HOST ?? 'localhost'
+    conf.database_user = process.env.PG_USER ?? 'twake'
+    conf.database_password = process.env.PG_PASSWORD ?? 'twake'
+    conf.database_name = process.env.PG_DATABASE ?? 'test'
+  }
+  buildUserDB(conf).then(() => {
+    idServer = new IdServer()
+    app = express()
+
+    idServer.ready.then(() => {
+      Object.keys(idServer.api.get).forEach(k => {
+        app.get(k, idServer.api.get[k])
+      })
+      Object.keys(idServer.api.post).forEach(k => {
+        app.post(k, idServer.api.post[k])
+      })
+      done()
+    }).catch(e => { done(e) })
+  }).catch(e => { done(e) })
+})
+
+beforeEach(() => {
+  jest.clearAllMocks()
+  jest.mock('node-fetch', () => jest.fn())
+  jest.mock('nodemailer', () => ({
+    createTransport: jest.fn().mockImplementation(() => ({
+      sendMail: sendMailMock
+    }))
+  }))
+})
+
+afterAll(() => {
+  if (process.env.TEST_PG !== 'yes') fs.unlinkSync('src/__testData__/test.db')
+  idServer.cleanJobs()
+})
+
+describe('/_matrix/identity/v2/account/register', () => {
+  it('should accept valid request', async () => {
+    const mockResponse = Promise.resolve({
+      ok: true,
+      status: 200,
+      json: () => {
+        return {
+          sub: '@dwho:example.com'
+        }
+      }
+    })
+    // @ts-expect-error mock is unknown
+    fetch.mockImplementation(async () => await mockResponse)
+    await mockResponse
+    const response = await request(app)
+      .post('/_matrix/identity/v2/account/register')
+      .send({
+        access_token: 'bar',
+        expires_in: 86400,
+        matrix_server_name: 'matrix.example.com',
+        token_type: 'Bearer'
+      })
+      .set('Accept', 'application/json')
+    expect(response.statusCode).toBe(200)
+    expect(response.body.token).toMatch(/^[a-zA-Z0-9]{64}$/)
+    validToken = response.body.token
+  })
+})
+
+describe('/_matrix/identity/v2/lookup', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let pepper = ''
+  describe('/_matrix/identity/v2/hash_details', () => {
+    it('should require authentication', async () => {
+      await idServer.cronTasks?.ready
+      const response = await request(app)
+        .get('/_matrix/identity/v2/hash_details')
+        .set('Accept', 'application/json')
+      expect(response.statusCode).toBe(401)
+    })
+    it('should display algorithms and pepper', async () => {
+      await idServer.cronTasks?.ready
+      const response = await request(app)
+        .get('/_matrix/identity/v2/hash_details')
+        .set('Authorization', `Bearer ${validToken}`)
+        .set('Accept', 'application/json')
+      expect(response.body).toHaveProperty('lookup_pepper')
+      expect(response.statusCode).toBe(200)
+      pepper = response.body.lookup_pepper
+      expect(response.body.algorithms).toEqual(supportedHashes)
+    })
+  })
+
+  describe('/_matrix/identity/v2/lookup', () => {
+    it('should return Matrix id', async () => {
+      const hash = new Hash()
+      await hash.ready
+      await idServer.cronTasks?.ready
+      const phoneHash = hash.sha256(`33612345678 phone ${pepper}`)
+      const response = await request(app)
+        .post('/_matrix/identity/v2/lookup')
+        .send({
+          addresses: [phoneHash],
+          algorithm: 'sha256',
+          pepper
+        })
+        .set('Authorization', `Bearer ${validToken}`)
+        .set('Accept', 'application/json')
+      expect(response.statusCode).toBe(200)
+      expect(response.body.mappings[phoneHash]).toBe('@dwho:matrix.org')
+    })
+  })
+})
+
+describe('/_matrix/identity/v2/account', () => {
+  it('should logout (/_matrix/identity/v2/account/logout)', async () => {
+    let response = await request(app)
+      .post('/_matrix/identity/v2/account/logout')
+      .set('Authorization', `Bearer ${validToken}`)
+      .set('Accept', 'application/json')
+    expect(response.statusCode).toBe(200)
+    response = await request(app)
+      .get('/_matrix/identity/v2/account')
+      .set('Authorization', `Bearer ${validToken}`)
+      .set('Accept', 'application/json')
+    expect(response.statusCode).toBe(401)
+  })
+})
