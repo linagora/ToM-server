e37369fad53ff2b2cae4669bf02b42e627922738
Yadd <yadd@debian.org>
2023-05-04 12:24:43 +0400

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,49 +1,40 @@
 import express from 'express'
-import IdServer from '@twake/identity-server'
-import TwakeVaultAPI from '@twake/vault-api'
+import TomServer from '@twake/server'
 import path from 'node:path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const vaultConfig = {
-  database_host: process.env.DATABASE_HOST,
+const tomServer = new TomServer({
+  base_url: process.env.BASE_URL,
+  cron_service: process.env.CRON_SERVICE ?? true,
   database_engine: process.env.DATABASE_ENGINE,
-  server_name: process.env.SERVER_NAME,
-  matrix_server: process.env.MATRIX_SERVER
-}
-const vaultApiServer = new TwakeVaultAPI(vaultConfig)
-
-const idServer = new IdServer({
-  ...vaultConfig,
+  database_host: process.env.DATABASE_HOST,
+  database_name: process.env.DATABASE_NAME,
   database_user: process.env.DATABASE_USER,
   database_password: process.env.DATABASE_PASSWORD,
-  database_name: process.env.DATABASE_NAME,
   ldap_base: process.env.LDAP_BASE,
+  ldap_filter: process.env.LDAP_FILTER,
   ldap_user: process.env.LDAP_USER,
   ldap_password: process.env.LDAP_PASSWORD,
   ldap_uri: process.env.LDAP_URI,
-  template_dir: path.join(__dirname,'packages','identity-server','templates'),
+  matrix_server: process.env.MATRIX_SERVER,
+  matrix_database_engine: process.env.MATRIX_DATABASE_ENGINE,
+  matrix_database_host: process.env.MATRIX_DATABASE_HOST,
+  matrix_database_name: process.env.MATRIX_DATABASE_NAME,
+  matrix_database_password: process.env.MATRIX_DATABASE_PASSWORD,
+  matrix_database_user: process.env.MATRIX_DATABASE_USER,
+  server_name: process.env.SERVER_NAME,
+  template_dir: path.join(__dirname,'packages','tom-server','templates'),
   userdb_engine: 'ldap',
 })
 
 const app = express()
 
-idServer.ready
+tomServer.ready
   .then(() => {
-    Object.keys(idServer.api.get).forEach((k) => {
-      console.log('Add GET endpoint', k)
-      app.get(k, idServer.api.get[k])
-    })
-    Object.keys(idServer.api.post).forEach((k) => {
-      console.log('Add POST endpoint', k)
-      app.post(k, idServer.api.post[k])
-    })
-    return vaultApiServer.ready
-  })
-  .then(() => {
-    app.use(vaultApiServer.endpoints)
+    app.use(tomServer.endpoints)
     const port = process.argv[2] != null ? parseInt(process.argv[2]) : 3000
     console.log(`Listening on port ${port}`)
     app.listen(port)
