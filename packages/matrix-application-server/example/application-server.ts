dfb4447509012487a91b35b4defc730b605f72fe
Jordy Cabannes <jcabannes@linagora.com>
2023-05-26 10:45:31 +0200

    feat: add example for matrix-application-server

---
+++ packages/matrix-application-server/example/application-server.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+
+import MatrixApplicationServer from '@twake/matrix-application-server'
+
+const appServer = new MatrixApplicationServer({
+  application_server_url: 'https://localhost:3000',
+  sender_localpart: 'foobot',
+  registration_file_path: 'registration.yaml',
+  namespaces: {}
+})
+
+const app = express()
+
+app.use(appServer.endpoints)
+
+const port = process.argv[2] != null ? parseInt(process.argv[2]) : 3000
+console.log(`Listening on port ${port}`)
+app.listen(port)
