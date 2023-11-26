61e50d50fa0e99cf26b5ed007a5ee3d40fcb0c35
Yadd <yadd@debian.org>
2023-11-26 18:40:08 +0400

    Update external dependencies

--- packages/tom-server/rollup.config.js
+++ packages/tom-server/rollup.config.js
@@ -2,7 +2,10 @@ import config from '../../rollup-template.js'
 
 export default config([
   'express',
+  'express-validator',
   'fs',
+  'ldapjs',
+  'lodash',
   'node-fetch',
   '@twake/config-parser',
   '@twake/matrix-identity-server',
