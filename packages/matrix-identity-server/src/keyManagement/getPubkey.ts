c0e9bf24875b99f8349215936b3018503efed0cd
Mathixx <147631380+Mathixx@users.noreply.github.com>
2024-06-17 17:05:36 +0400

    fix: synthax corrected on added and modified files

--- packages/matrix-identity-server/src/keyManagement/getPubkey.ts
+++ packages/matrix-identity-server/src/keyManagement/getPubkey.ts
@@ -1,48 +1,45 @@
-import type IdentityServerDB from '../db';
-import { type Request } from 'express';
-import { type TwakeLogger } from '@twake/logger';
-import {
-  send,
-  type expressAppHandler
-} from '../utils';
-import { errMsg } from '../utils/errors';
+import type IdentityServerDB from '../db'
+import { type Request } from 'express'
+import { type TwakeLogger } from '@twake/logger'
+import { send, type expressAppHandler } from '../utils'
+import { errMsg } from '../utils/errors'
 
 const getPubkey = (
   idServer: IdentityServerDB,
   logger: TwakeLogger
 ): expressAppHandler => {
   return (req, res) => {
-    console.log("test entry")
-    const _keyID: string = (req as Request).params.keyId;
+    console.log('test entry')
+    const _keyID: string = (req as Request).params.keyId
 
     if (_keyID.length === 0) {
-      send(res, 400, errMsg('missingParams', 'keyId'));
+      send(res, 400, errMsg('missingParams', 'keyId'))
     } else {
       idServer.db
-      .get('shortTermKeypairs', ['public'], { keyID : _keyID })
-      // eslint-disable-next-line @typescript-eslint/promise-function-async
-      .then((rows) => {
-        if (rows.length === 1) {
-          send(res, 200, { public_key: rows[0].public });
-        } else {
-          return idServer.db
-            .get('longTermKeypairs', ['public'], { keyID : _keyID })
-            .then((rows) => {
-              if (rows.length === 0) {
-                send(res, 404, errMsg('notFound'));
-              } else {
-                send(res, 200, { public_key: rows[0].public });
-              }
-            });
-        }
-      })
-      .catch((e) => {
-        console.error("Error querying keypairs:", e); // Debugging statement
-        /* istanbul ignore next */
-        send(res, 500, errMsg('unknown', e));
-      });
+        .get('shortTermKeypairs', ['public'], { keyID: _keyID })
+        // eslint-disable-next-line @typescript-eslint/promise-function-async
+        .then((rows) => {
+          if (rows.length === 1) {
+            send(res, 200, { public_key: rows[0].public })
+          } else {
+            return idServer.db
+              .get('longTermKeypairs', ['public'], { keyID: _keyID })
+              .then((rows) => {
+                if (rows.length === 0) {
+                  send(res, 404, errMsg('notFound'))
+                } else {
+                  send(res, 200, { public_key: rows[0].public })
+                }
+              })
+          }
+        })
+        .catch((e) => {
+          console.error('Error querying keypairs:', e) // Debugging statement
+          /* istanbul ignore next */
+          send(res, 500, errMsg('unknown', e))
+        })
     }
   }
 }
 
-export default getPubkey;
+export default getPubkey
