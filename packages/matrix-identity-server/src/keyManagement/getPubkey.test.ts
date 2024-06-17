8185f62ec054613bb36da46a68909a9e08699bba
Mathixx <147631380+Mathixx@users.noreply.github.com>
2024-06-17 17:05:36 +0400

    fix: synthax corrected on added and modified files

--- packages/matrix-identity-server/src/keyManagement/getPubkey.test.ts
+++ packages/matrix-identity-server/src/keyManagement/getPubkey.test.ts
@@ -8,16 +8,16 @@ import { type Config } from '../types'
 import defaultConfig from '../__testData__/registerConf.json'
 import getPubkey from './getPubkey'
 
-
 let app: express.Application
 let conf: Config
 
 const logger: TwakeLogger = getLogger()
-const longKeyPair : { publicKey: string, privateKey: string, keyId: string }  = generateKeyPair('ed25519')
-const shortKeyPair : { publicKey: string, privateKey: string, keyId: string }  = generateKeyPair('curve25519')
-console.log("longKeyPair", longKeyPair)
-console.log("shortKeyPair", shortKeyPair)
-
+const longKeyPair: { publicKey: string; privateKey: string; keyId: string } =
+  generateKeyPair('ed25519')
+const shortKeyPair: { publicKey: string; privateKey: string; keyId: string } =
+  generateKeyPair('curve25519')
+console.log('longKeyPair', longKeyPair)
+console.log('shortKeyPair', shortKeyPair)
 
 beforeAll(async () => {
   conf = {
@@ -37,69 +37,72 @@ afterAll(() => {
 })
 
 describe('Get public key from keyID', () => {
-    let db: IdentityServerDB
-  
-    beforeAll(async () => {
-        app = express()
-        db = new IdentityServerDB(conf, logger)
-  
-        await db.ready
-        app.get(`/_matrix/identity/v2/pubkey/:keyId`, getPubkey(db, logger))
-  
-        // Insert a test key into the database
-        await db.insert('longTermKeypairs', {
-            keyID: longKeyPair.keyId,
-            public: longKeyPair.publicKey,
-            private: longKeyPair.privateKey
-        })
-        await db.insert('shortTermKeypairs', {
-            keyID: shortKeyPair.keyId,
-            public: shortKeyPair.publicKey,
-            private: shortKeyPair.privateKey
-        })
+  let db: IdentityServerDB
+
+  beforeAll(async () => {
+    app = express()
+    db = new IdentityServerDB(conf, logger)
+
+    await db.ready
+    app.get(`/_matrix/identity/v2/pubkey/:keyId`, getPubkey(db, logger))
+
+    // Insert a test key into the database
+    await db.insert('longTermKeypairs', {
+      keyID: longKeyPair.keyId,
+      public: longKeyPair.publicKey,
+      private: longKeyPair.privateKey
     })
-  
-    afterAll(async () => {
-      clearTimeout(db.cleanJob)
-      // Clean up test database file
-      if (fs.existsSync('./src/__testData__/hashes.db')) {
-        fs.unlinkSync('./src/__testData__/hashes.db')
-      }
-      db.close()
-      logger.close()
+    await db.insert('shortTermKeypairs', {
+      keyID: shortKeyPair.keyId,
+      public: shortKeyPair.publicKey,
+      private: shortKeyPair.privateKey
     })
-  
-    it('should return the public key when correct keyID is given (from long term key pairs)', async () => {
-      console.log("correct long")
-        const _keyID = longKeyPair.keyId
-        const response = await request(app)
-          .get(`/_matrix/identity/v2/pubkey/${_keyID}`)
+  })
 
-        expect(response.statusCode).toBe(200)
-        expect(response.body.public_key).toBeDefined()
-        expect(response.body.public_key).toMatch(/^[A-Za-z0-9+/=]+$/)
-        expect(response.body.public_key).toBe(longKeyPair.publicKey)
-    })
+  afterAll(async () => {
+    clearTimeout(db.cleanJob)
+    // Clean up test database file
+    if (fs.existsSync('./src/__testData__/hashes.db')) {
+      fs.unlinkSync('./src/__testData__/hashes.db')
+    }
+    db.close()
+    logger.close()
+  })
 
-    it('should return the public key when correct keyID is given (from short term key pairs)', async () => {
-      console.log("correct short")
-        const _keyID = shortKeyPair.keyId
-        const response = await request(app)
-          .get(`/_matrix/identity/v2/pubkey/${_keyID}`)
+  it('should return the public key when correct keyID is given (from long term key pairs)', async () => {
+    console.log('correct long')
+    const _keyID = longKeyPair.keyId
+    const response = await request(app).get(
+      `/_matrix/identity/v2/pubkey/${_keyID}`
+    )
 
-        expect(response.statusCode).toBe(200)
-        expect(response.body.public_key).toBeDefined()
-        expect(response.body.public_key).toMatch(/^[A-Za-z0-9+/=]+$/)
-        expect(response.body.public_key).toBe(shortKeyPair.publicKey)
-    })
+    expect(response.statusCode).toBe(200)
+    expect(response.body.public_key).toBeDefined()
+    expect(response.body.public_key).toMatch(/^[A-Za-z0-9+/=]+$/)
+    expect(response.body.public_key).toBe(longKeyPair.publicKey)
+  })
 
-    it('should return 404 when incorrect keyID is given', async () => {
-      console.log("incorrect")
-        const _keyID = 'incorrectKeyID'
-        const response = await request(app)
-          .get(`/_matrix/identity/v2/pubkey/${_keyID}`) // exactement '/_matrix/identity/v2/pubkey/' + _keyID
+  it('should return the public key when correct keyID is given (from short term key pairs)', async () => {
+    console.log('correct short')
+    const _keyID = shortKeyPair.keyId
+    const response = await request(app).get(
+      `/_matrix/identity/v2/pubkey/${_keyID}`
+    )
 
-        expect(response.statusCode).toBe(404)
-        expect(response.body.errcode).toBe('M_NOT_FOUND')
-    })    
+    expect(response.statusCode).toBe(200)
+    expect(response.body.public_key).toBeDefined()
+    expect(response.body.public_key).toMatch(/^[A-Za-z0-9+/=]+$/)
+    expect(response.body.public_key).toBe(shortKeyPair.publicKey)
+  })
+
+  it('should return 404 when incorrect keyID is given', async () => {
+    console.log('incorrect')
+    const _keyID = 'incorrectKeyID'
+    const response = await request(app).get(
+      `/_matrix/identity/v2/pubkey/${_keyID}`
+    ) // exactement '/_matrix/identity/v2/pubkey/' + _keyID
+
+    expect(response.statusCode).toBe(404)
+    expect(response.body.errcode).toBe('M_NOT_FOUND')
+  })
 })
