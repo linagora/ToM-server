7ee915864f6c1b0dc6d501a4389bb5990b7a6def
Mathixx <147631380+Mathixx@users.noreply.github.com>
2024-06-19 17:55:58 +0400

    fix: key_management fixed

--- packages/matrix-identity-server/src/keyManagement/validEphemeralPubkey.ts
+++ packages/matrix-identity-server/src/keyManagement/validEphemeralPubkey.ts
@@ -1,44 +1,38 @@
 import type IdentityServerDB from '../db'
-import { type TwakeLogger, getLogger } from '@twake/logger'
-import {
-  jsonContent,
-  send,
-  validateParameters,
-  type expressAppHandler
-} from '../utils'
+import { type Request } from 'express'
+import { send, type expressAppHandler } from '../utils'
 import { errMsg } from '../utils/errors'
-
-const schema = {
-  public_key: true
-}
-
-const logger: TwakeLogger = getLogger()
 
 const isEphemeralPubkeyValid = (
   idServer: IdentityServerDB
 ): expressAppHandler => {
   return (req, res) => {
-    jsonContent(req, res, logger, (obj) => {
-      validateParameters(res, schema, obj, logger, (obj) => {
-        logger.debug(`request to search ${JSON.stringify(obj)}`)
-        idServer.db
-          .get('shortTermKeypairs', ['public'], {
-            public: (obj as { public_key: string }).public_key
-          })
-          .then((rows) => {
-            if (rows.length === 0) {
-              send(res, 200, { valid: false })
-            } else {
-              // On verifie ailleurs que la clef publique n'apparait bien qu'une seule fois !
-              send(res, 200, { valid: true })
-            }
-          })
-          .catch((e) => {
-            /* istanbul ignore next */
-            send(res, 500, errMsg('unknown', e))
-          })
-      })
-    })
+    const publicKey = (req as Request).query.public_key
+    console.log('publicKey ephemeral:', publicKey)
+    if (
+      publicKey !== undefined &&
+      typeof publicKey === 'string' &&
+      publicKey.length > 0
+    ) {
+      idServer.db
+        .get('shortTermKeypairs', ['public'], {
+          public: publicKey
+        })
+        .then((rows) => {
+          if (rows.length === 0) {
+            send(res, 200, { valid: false })
+          } else {
+            // TO DO : ensure that the pubkey only appears one time
+            send(res, 200, { valid: true })
+          }
+        })
+        .catch((e) => {
+          /* istanbul ignore next */
+          send(res, 500, errMsg('unknown', e))
+        })
+    } else {
+      send(res, 400, errMsg('missingParams'))
+    }
   }
 }
 
