fd55bba1923c022b6807761faab49f915fc12223
Mathixx <147631380+Mathixx@users.noreply.github.com>
2024-06-19 12:53:27 +0400

    feat: added createKeypair method for the identityServer

--- packages/matrix-identity-server/src/db/index.test.ts
+++ packages/matrix-identity-server/src/db/index.test.ts
@@ -33,6 +33,9 @@ describe('Id Server DB', () => {
   afterAll(() => {
     idDb.close()
     logger.close()
+    // if (fs.existsSync('./testdb.db')) {
+    //   fs.unlinkSync('./testdb.db')
+    // }
   })
   it('should have SQLite database initialized', (done) => {
     idDb = new IdDb(baseConf, logger)
@@ -549,28 +552,33 @@ describe('Id Server DB', () => {
       .catch((e) => done(e))
   })
 
-  it('should delete a key from the shortKey pairs table', (done) => {
+  it('should delete a key from the correct table', (done) => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     idDb = new IdDb(baseConf, logger)
     idDb.ready
       .then(() => {
         idDb
-          .createKeypair('shortTerm', 'ed25519')
+          .createKeypair('longTerm', 'ed25519')
           .then((key1) => {
             idDb
               .createKeypair('shortTerm', 'curve25519')
               .then((key2) => {
                 idDb
-                  .deleteKey(key1.keyId)
+                  .deleteKey(key1.keyId, 'longTerm')
                   .then(() => {
                     idDb
-                      .get('shortTermKeypairs', ['keyID'], {})
+                      .get('longTermKeypairs', ['keyID'], {})
                       .then((rows) => {
-                        expect(rows.length).toBe(1)
-                        expect(rows[0].keyID).toEqual(key2.keyId)
-                        clearTimeout(idDb.cleanJob)
-                        idDb.close()
-                        done()
+                        expect(rows.length).toBe(0)
+                        idDb
+                          .get('shortTermKeypairs', ['keyID'], {})
+                          .then((rows) => {
+                            expect(rows.length).toBe(1)
+                            clearTimeout(idDb.cleanJob)
+                            idDb.close()
+                            done()
+                          })
+                          .catch((e) => done(e))
                       })
                       .catch((e) => done(e))
                   })
