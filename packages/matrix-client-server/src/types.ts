91a2328b7d91e4d31a4732610ef75e65dd933c3c
Jordy CABANNES <jordycabannes@msn.com>
2024-06-25 15:17:12 +0200

    feat: init matrix-client-server

---
+++ packages/matrix-client-server/src/types.ts
@@ -0,0 +1,8 @@
+import {
+  type Config as MIdentityServerConfig,
+  type IdentityServerDb as MIdentityServerDb
+} from '@twake/matrix-identity-server'
+
+export type Config = MIdentityServerConfig
+
+export type Collections = MIdentityServerDb.Collections
