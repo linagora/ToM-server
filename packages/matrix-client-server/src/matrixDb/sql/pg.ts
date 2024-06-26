ed0c1510594d660d8a4fadb2f07310a1bc156cb7
Mathixx <147631380+Mathixx@users.noreply.github.com>
2024-06-26 16:51:25 +0400

    feat: new matrixDB added for client server which enables insertion, etc

---
+++ packages/matrix-client-server/src/matrixDb/sql/pg.ts
@@ -0,0 +1,64 @@
+import { type TwakeLogger } from '@twake/logger'
+import { type ClientConfig } from 'pg'
+import { type Config } from '../../types'
+import { type MatrixDBmodifiedBackend } from '../'
+import { type Collections } from '../../../../matrix-identity-server/src/db'
+import Pg from '../../../../matrix-identity-server/src/db/sql/pg'
+
+class MatrixDBPg extends Pg implements MatrixDBmodifiedBackend {
+  // eslint-disable-next-line @typescript-eslint/promise-function-async
+  createDatabases(
+    conf: Config,
+    tables: Record<Collections, string>,
+    indexes: Partial<Record<Collections, string[]>>,
+    initializeValues: Partial<
+      Record<Collections, Array<Record<string, string | number>>>
+    >,
+    logger: TwakeLogger
+  ): Promise<void> {
+    if (this.db != null) return Promise.resolve()
+    return new Promise((resolve, reject) => {
+      import('pg')
+        .then((pg) => {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment, @typescript-eslint/prefer-ts-expect-error
+          // @ts-ignore
+          if (pg.Database == null) pg = pg.default
+          if (
+            conf.matrix_database_host == null ||
+            conf.matrix_database_user == null ||
+            conf.matrix_database_password == null ||
+            conf.matrix_database_name == null
+          ) {
+            throw new Error(
+              'database_name, database_user and database_password are required when using Postgres'
+            )
+          }
+          const opts: ClientConfig = {
+            host: conf.matrix_database_host,
+            user: conf.matrix_database_user,
+            password: conf.matrix_database_password,
+            database: conf.matrix_database_name,
+            ssl: conf.matrix_database_ssl
+          }
+          // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+          if (conf.matrix_database_host.match(/^(.*):(\d+)/)) {
+            opts.host = RegExp.$1
+            opts.port = parseInt(RegExp.$2)
+          }
+          try {
+            this.db = new pg.Pool(opts)
+            resolve()
+          } catch (e) {
+            logger.error('Unable to connect to Pg database')
+            reject(e)
+          }
+        })
+        .catch((e) => {
+          logger.error('Unable to load pg module')
+          reject(e)
+        })
+    })
+  }
+}
+
+export default MatrixDBPg
--- packages/matrix-client-server/src/types.ts
+++ packages/matrix-client-server/src/types.ts
@@ -6,3 +6,22 @@ import {
 export type Config = MIdentityServerConfig
 
 export type Collections = MIdentityServerDb.Collections
+
+export type DbGetResult = Array<
+  Record<string, string | number | Array<string | number>>
+>
+
+export interface LocalMediaRepository {
+  media_id: string
+  media_length: string
+  user_id: string
+}
+
+export interface MatrixUser {
+  name: string
+}
+
+export interface UserQuota {
+  user_id: string
+  size: number
+}
