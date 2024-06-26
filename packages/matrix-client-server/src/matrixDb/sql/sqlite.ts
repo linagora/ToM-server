f6d21a82f51258e1da0d25440b4a66e1eb73f1d5
Mathixx <147631380+Mathixx@users.noreply.github.com>
2024-06-26 16:51:25 +0400

    feat: new matrixDB added for client server which enables insertion, etc

---
+++ packages/matrix-client-server/src/matrixDb/sql/sqlite.ts
@@ -0,0 +1,41 @@
+import { type Collections } from '../../../../matrix-identity-server/src/db'
+import { type MatrixDBmodifiedBackend } from '../'
+import { type Config } from '../../types'
+import SQLite from '../../../../matrix-identity-server/src/db/sql/sqlite'
+
+class MatrixDBSQLite extends SQLite implements MatrixDBmodifiedBackend {
+  // eslint-disable-next-line @typescript-eslint/promise-function-async
+  createDatabases(
+    conf: Config,
+    tables: Record<Collections, string>,
+    indexes: Partial<Record<Collections, string[]>>,
+    initializeValues: Partial<
+      Record<Collections, Array<Record<string, string | number>>>
+    >
+  ): Promise<void> {
+    if (this.db != null) return Promise.resolve()
+    return new Promise((resolve, reject) => {
+      import('sqlite3')
+        .then((sqlite3) => {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment, @typescript-eslint/prefer-ts-expect-error
+          /* istanbul ignore next */ // @ts-ignore
+          if (sqlite3.Database == null) sqlite3 = sqlite3.default
+          const db = (this.db = new sqlite3.Database(
+            conf.matrix_database_host as string,
+            sqlite3.OPEN_READONLY
+          ))
+          /* istanbul ignore if */
+          if (db == null) {
+            reject(new Error('Database not created'))
+          }
+          resolve()
+        })
+        .catch((e) => {
+          /* istanbul ignore next */
+          reject(e)
+        })
+    })
+  }
+}
+
+export default MatrixDBSQLite
--- packages/matrix-client-server/src/types.ts
+++ packages/matrix-client-server/src/types.ts
@@ -1,3 +1,24 @@
 import { type Config as MIdentityServerConfig } from '@twake/matrix-identity-server'
 
 export type Config = MIdentityServerConfig
+
+export type Collections = MIdentityServerDb.Collections
+
+export type DbGetResult = Array<
+  Record<string, string | number | Array<string | number>>
+>
+
+export interface LocalMediaRepository {
+  media_id: string
+  media_length: string
+  user_id: string
+}
+
+export interface MatrixUser {
+  name: string
+}
+
+export interface UserQuota {
+  user_id: string
+  size: number
+}
