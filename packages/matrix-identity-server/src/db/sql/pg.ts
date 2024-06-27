5545e4c2241e5dfc21f08eab9e9e67cf6220d2e7
Mathixx <147631380+Mathixx@users.noreply.github.com>
2024-06-27 12:30:21 +0400

    fix: Client Server's matrixDB fixed and __testData__ folder added and configured to support new matrixDB

--- packages/matrix-identity-server/src/db/sql/pg.ts
+++ packages/matrix-identity-server/src/db/sql/pg.ts
@@ -171,7 +171,7 @@ class Pg<T extends string> extends SQL<T> implements IdDbBackend<T> {
   }
 
   update(
-    table: T,
+    table: string,
     values: Record<string, string | number>,
     field: string,
     value: string | number
