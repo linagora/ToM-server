68f40f10459b0aabbeaa2e5758da21df10f371ea
Jordy CABANNES <jordycabannes@msn.com>
2024-06-29 13:30:26 +0200

    fix: remove useless db folder in matrix-client-server

--- packages/matrix-client-server/src/types.ts
+++ packages/matrix-client-server/src/types.ts
@@ -1,8 +1,3 @@
-import {
-  type Config as MIdentityServerConfig,
-  type IdentityServerDb as MIdentityServerDb
-} from '@twake/matrix-identity-server'
+import { type Config as MIdentityServerConfig } from '@twake/matrix-identity-server'
 
 export type Config = MIdentityServerConfig
-
-export type Collections = MIdentityServerDb.Collections
