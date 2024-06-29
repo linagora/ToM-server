803d0b6f5ccedc12653223ef45f439e1cc7c0c6e
Jordy CABANNES <jordycabannes@msn.com>
2024-06-29 12:58:21 +0200

    fix: format imports in matrix-identity-server

--- packages/matrix-identity-server/src/3pid/unbind.ts
+++ packages/matrix-identity-server/src/3pid/unbind.ts
@@ -1,11 +1,11 @@
-import {
-  type expressAppHandler,
-  jsonContent,
-  send,
-  validateParameters
-} from '../utils'
 import type MatrixIdentityServer from '..'
 import { errMsg } from '..'
+import {
+  jsonContent,
+  send,
+  validateParameters,
+  type expressAppHandler
+} from '../utils'
 
 const clientSecretRe = /^[0-9a-zA-Z.=_-]{6,255}$/
 const mxidRe = /^@[0-9a-zA-Z._=-]+:[0-9a-zA-Z.-]+$/
