cd2e6f1cd38ec0620cf090f277d38bff74e22b4b
BichraiX <160038319+BichraiX@users.noreply.github.com>
2024-07-01 11:59:50 +0400

    fix : modified authenticate function to comply with the changes in identity-server

--- packages/matrix-identity-server/src/db/sql/pg.ts
+++ packages/matrix-identity-server/src/db/sql/pg.ts
@@ -171,7 +171,7 @@ class Pg<T extends string> extends SQL<T> implements IdDbBackend<T> {
   }
 
   update(
-    table: string,
+    table: T,
     values: Record<string, string | number>,
     field: string,
     value: string | number
