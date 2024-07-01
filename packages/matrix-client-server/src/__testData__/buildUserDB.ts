85cee39386fbb2925212347f194ae195e85652a0
Mathixx <147631380+Mathixx@users.noreply.github.com>
2024-07-01 16:52:12 +0400

    fix: added structure of index.test.ts + modified buildMatrixDB

--- packages/matrix-client-server/src/__testData__/buildUserDB.ts
+++ packages/matrix-client-server/src/__testData__/buildUserDB.ts
@@ -15,11 +15,49 @@ const createQuery = 'CREATE TABLE IF NOT EXISTS users (uid varchar(8), mobile va
 const insertQuery = "INSERT INTO users VALUES('dwho', '33612345678', 'dwho@company.com')"
 const insertQuery2 = "INSERT INTO users VALUES('rtyler', '33687654321', 'rtyler@company.com')"
 
-const createQueryMat1 = 'CREATE TABLE IF NOT EXISTS profiles( user_id TEXT NOT NULL, displayname TEXT, avatar_url TEXT, UNIQUE(user_id) )'
-const insertQueryMat1 = "INSERT INTO profiles VALUES('dwho', 'D Who', 'http://example.com/avatar.jpg')"
+const matrixDbQueries = [
+  'CREATE TABLE IF NOT EXISTS profiles( user_id TEXT NOT NULL, displayname TEXT, avatar_url TEXT, UNIQUE(user_id) )',
+  "INSERT INTO profiles VALUES('dwho', 'D Who', 'http://example.com/avatar.jpg')",
+  'CREATE TABLE IF NOT EXISTS users (user_id TEXT NOT NULL, device_id TEXT NOT NULL, PRIMARY KEY (user_id, device_id))',
+]
 
 // eslint-disable-next-line @typescript-eslint/promise-function-async
-const buildUserDB = (conf: Config): Promise<void> => {
+const runQueries = (
+  db: sqlite3.Database | any,
+  queries: string[],
+  isSqlite: boolean
+): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    const runNextQuery = (index: number): void => {
+      if (index >= queries.length) {
+        resolve();
+      } else {
+        if (isSqlite) {
+          db.run(queries[index], (err: Error | null) => {
+            if (err) {
+              reject(err);
+            } else {
+              runNextQuery(index + 1);
+            }
+          });
+        } else {
+          db.query(queries[index], (err: Error | null) => {
+            if (err) {
+              reject(err);
+            } else {
+              runNextQuery(index + 1);
+            }
+          });
+        }
+      }
+    };
+    runNextQuery(0);
+  });
+};
+
+
+// eslint-disable-next-line @typescript-eslint/promise-function-async
+export const buildUserDB = (conf: Config): Promise<void> => {
   if (created) return Promise.resolve()
   const userDb = new UserDB(conf, logger)
   return new Promise((resolve, reject) => {
@@ -55,26 +93,30 @@ const buildUserDB = (conf: Config): Promise<void> => {
 
 // eslint-disable-next-line @typescript-eslint/promise-function-async
 export const buildMatrixDb = (conf: Config): Promise<void> => {
-  if (matrixDbCreated) return Promise.resolve()
-  const matrixDb = new sqlite3.Database(conf.matrix_database_host as string)
+  if (matrixDbCreated) return Promise.resolve();
+  const matrixDb = new sqlite3.Database(conf.matrix_database_host as string) 
   return new Promise((resolve, reject) => {
-      /* istanbul ignore else */
-      if (conf.matrix_database_engine === 'sqlite') {
-      matrixDb.run(createQueryMat1, () => {
-        matrixDb.run(insertQueryMat1).close((err) => {
-          /* istanbul ignore if */
-          if(err != null) {
-            reject(err)
-          } else {
-            matrixDbCreated = true
-            resolve()
-          }
+    if (conf.matrix_database_engine === 'sqlite') {
+      runQueries(matrixDb, matrixDbQueries, true)
+        .then(() => {
+          matrixDb.close((err) => {
+            if (err) {
+              reject(err);
+            } else {
+              matrixDbCreated = true;
+              resolve();
+            }
+          });
         })
-      })
+        .catch((err) => {
+          matrixDb.close(() => {
+            reject(err);
+          });
+        });
     } else {
-      throw new Error('only SQLite is implemented here')
+      matrixDb.close(() => {
+        reject(new Error('only SQLite is implemented here'));
+      });
     }
-  })
-}
-
-export default buildUserDB
+  });
+};
