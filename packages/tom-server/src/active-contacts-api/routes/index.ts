ec965e0a0770c6cd79db0e6f1feb2e6847f1d4e5
Khaled FERJANI <kferjani@linagora.com>
2024-07-08 19:53:49 +0100

    📝 feat: added swagger docs

--- packages/tom-server/src/active-contacts-api/routes/index.ts
+++ packages/tom-server/src/active-contacts-api/routes/index.ts
@@ -31,8 +31,74 @@ export default (
   const validationMiddleware = new ActiveContactsApiValidationMiddleWare()
   const router = Router()
 
+  /**
+   * @openapi
+   * components:
+   *  schemas:
+   *    ActiveContacts:
+   *      type: object
+   *      description: the list of active contacts
+   *      properties:
+   *        contacts:
+   *          type: string
+   *          description: active contacts
+   *  responses:
+   *    NotFound:
+   *      description: no active contacts found
+   *    Unauthorized:
+   *      description: the user is not authorized
+   *    Created:
+   *      description: active contacts saved
+   *    NoContent:
+   *      description: operation successful and no content returned
+   */
+
+  /**
+   * @openapi
+   * /_twake/v1/activecontacts:
+   *  get:
+   *    tags:
+   *      - Active contacts
+   *    description: Get the list of active contacts
+   *    responses:
+   *      200:
+   *        description: Active contacts found
+   *        content:
+   *          application/json:
+   *            schema:
+   *                $ref: '#/components/schemas/ActiveContacts'
+   *      404:
+   *        description: Active contacts not found
+   *      401:
+   *        description: user is unauthorized
+   *      500:
+   *        description: Internal error
+   */
   router.get(PATH, authenticate, activeContactsApiController.get)
 
+  /**
+   * @openapi
+   * /_twake/v1/activecontacts:
+   *  post:
+   *    tags:
+   *      - Active contacts
+   *    description: Create or update the list of active contacts
+   *    requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *          schema:
+   *            $ref: '#/components/schemas/ActiveContacts'
+   *    responses:
+   *      201:
+   *        description: Active contacts saved
+   *      401:
+   *        description: user is unauthorized
+   *      400:
+   *        description: Bad request
+   *      500:
+   *        description: Internal error
+   */
   router.post(
     PATH,
     authenticate,
@@ -40,6 +106,21 @@ export default (
     activeContactsApiController.save
   )
 
+  /**
+   * @openapi
+   * /_twake/v1/activecontacts:
+   *  delete:
+   *    tags:
+   *      - Active contacts
+   *    description: Delete the list of active contacts
+   *    responses:
+   *      200:
+   *        description: Active contacts deleted
+   *      401:
+   *        description: user is unauthorized
+   *      500:
+   *        description: Internal error/
+   */
   router.delete(PATH, authenticate, activeContactsApiController.delete)
 
   return router
