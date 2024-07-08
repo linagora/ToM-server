8e1b5f20967825f246638a717fc2935dbe8cbe11
Khaled FERJANI <kferjani@linagora.com>
2024-07-08 14:18:35 +0100

    🧪 feat: added tests

---
+++ packages/tom-server/src/active-contacts-api/tests/routes.test.ts
@@ -0,0 +1,139 @@
+import express, { type Response, type NextFunction } from 'express'
+import bodyParser from 'body-parser'
+import type { AuthRequest, Config } from '../../types'
+import IdServer from '../../identity-server'
+import type { ConfigDescription } from '@twake/config-parser'
+import type { TwakeLogger } from '@twake/logger'
+import { IdentityServerDb, type MatrixDB } from '@twake/matrix-identity-server'
+import router, { PATH } from '../routes'
+import errorMiddleware from '../../utils/middlewares/error.middleware'
+import JEST_PROCESS_ROOT_PATH from '../../../jest.globals'
+import fs from 'fs'
+import path from 'path'
+import supertest from 'supertest'
+
+const mockLogger: Partial<TwakeLogger> = {
+  debug: jest.fn(),
+  error: jest.fn(),
+  warn: jest.fn(),
+  close: jest.fn()
+}
+
+jest
+  .spyOn(IdentityServerDb.default.prototype, 'get')
+  .mockResolvedValue([{ data: '"test"' }])
+
+const idServer = new IdServer(
+  {
+    get: jest.fn()
+  } as unknown as MatrixDB,
+  {} as unknown as Config,
+  {
+    database_engine: 'sqlite',
+    database_host: 'test.db',
+    rate_limiting_window: 10000,
+    rate_limiting_nb_requests: 100,
+    template_dir: './templates',
+    userdb_host: './tokens.db'
+  } as unknown as ConfigDescription,
+  mockLogger as TwakeLogger
+)
+
+const app = express()
+const middlewareSpy = jest.fn().mockImplementation((_req, _res, next) => {
+  next()
+})
+
+jest.mock('../middlewares', () => {
+  return function () {
+    return {
+      checkCreationRequirements: middlewareSpy
+    }
+  }
+})
+
+jest.mock('../controllers', () => {
+  const passiveController = (
+    _req: AuthRequest,
+    res: Response,
+    _next: NextFunction
+  ): void => {
+    res.status(200).json({ message: 'test' })
+  }
+
+  return function () {
+    return {
+      get: passiveController,
+      save: passiveController,
+      delete: passiveController
+    }
+  }
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+describe('The active contacts API router', () => {
+  beforeAll((done) => {
+    idServer.ready
+      .then(() => {
+        app.use(
+          router(
+            idServer.db,
+            idServer.conf,
+            idServer.authenticate,
+            idServer.logger
+          )
+        )
+
+        app.use(errorMiddleware(idServer.logger))
+        done()
+      })
+      .catch((e) => {
+        done(e)
+      })
+  })
+
+  afterAll(() => {
+    idServer.cleanJobs()
+
+    const pathFilesToDelete = [
+      path.join(JEST_PROCESS_ROOT_PATH, 'test.db'),
+      path.join(JEST_PROCESS_ROOT_PATH, 'tokens.db')
+    ]
+
+    pathFilesToDelete.forEach((path) => {
+      if (fs.existsSync(path)) fs.unlinkSync(path)
+    })
+  })
+
+  it('should reject if rate limit is exceeded', async () => {
+    let response
+
+    for (let i = 0; i < 101; i++) {
+      response = await supertest(app).get(PATH).set('Authorization', 'Bearer test')
+    }
+
+    expect((response as unknown as Response).status).toEqual(429)
+    await new Promise((resolve) => setTimeout(resolve, 11000))
+  })
+
+  it('should not call the validation middleware if the Bearer token is not set', async () => {
+    const response = await supertest(app).get(PATH)
+
+    expect(response.status).toEqual(401)
+    expect(middlewareSpy).not.toHaveBeenCalled()
+  })
+
+  it('should call the validation middleware if the Bearer token is set', async () => {
+    await supertest(app).get(PATH).set('Authorization', 'Bearer test')
+
+    expect(middlewareSpy).toHaveBeenCalled()
+  })
+
+  it('should call the validation middleware if the access_token is set in the query', async () => {
+    await supertest(app).get(PATH).query({ access_token: 'test' })
+
+    expect(middlewareSpy).toHaveBeenCalled()
+  })
+})
