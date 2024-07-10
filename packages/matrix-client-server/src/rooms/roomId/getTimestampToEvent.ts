69f7894071aae28fe68a0bb58c6b104c48c604ed
h1pp0X <hippolyte.wallaert@polytechnique.edu>
2024-07-10 14:48:26 +0400

    feat : added getTimestampToEvent api

---
+++ packages/matrix-client-server/src/rooms/roomId/getTimestampToEvent.ts
@@ -0,0 +1,85 @@
+import MatrixClientServer from '../..'
+import { errMsg, expressAppHandler, send } from '@twake/utils'
+
+interface query_parameters {
+  dir: 'b' | 'f'
+  ts: number
+}
+
+const GetTimestampToEvent = (
+  ClientServer: MatrixClientServer
+): expressAppHandler => {
+  return (req, res) => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const roomId: string = (req as Request).params.roomId
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const params: query_parameters = (req as Request).query
+    if (params.dir !== 'b' && params.dir !== 'f') {
+      send(res, 400, errMsg('invalidParam', 'Invalid parameters'))
+      return
+    }
+    // TODO : introduce rate-limiting
+    ClientServer.authenticate(req, res, (data, id) => {
+      if (params.dir === 'b') {
+        ClientServer.matrixDb
+          .getMaxWhereEqualAndLower(
+            'events',
+            'origin_server_ts',
+            ['event_id', 'origin_server_ts'],
+            {
+              room_id: roomId
+            },
+            {
+              origin_server_ts: params.ts
+            }
+          )
+          .then((rows) => {
+            console.log(rows)
+            if (rows.length === 0) {
+              send(
+                res,
+                404,
+                errMsg(
+                  'notFound',
+                  `Unable to find event from ${params.ts} in backward direction`
+                )
+              )
+              return
+            }
+            send(res, 200, rows[0])
+          })
+      }
+      if (params.dir === 'f') {
+        ClientServer.matrixDb
+          .getMinWhereEqualAndHigher(
+            'events',
+            'origin_server_ts',
+            ['event_id', 'origin_server_ts'],
+            {
+              room_id: roomId
+            },
+            { origin_server_ts: params.ts }
+          )
+          .then((rows) => {
+            console.log(rows)
+            if (rows.length === 0) {
+              send(
+                res,
+                404,
+                errMsg(
+                  'notFound',
+                  `Unable to find event from ${params.ts} in forward direction`
+                )
+              )
+              return
+            }
+            send(res, 200, rows[0])
+          })
+      }
+    })
+  }
+}
+
+export default GetTimestampToEvent
