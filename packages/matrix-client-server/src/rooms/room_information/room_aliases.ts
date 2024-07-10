71c9cb3830f220d62336a882a51193d92fb3342a
Mathixx <147631380+Mathixx@users.noreply.github.com>
2024-07-10 14:53:30 +0400

    fix: finished endpoint and tests

--- packages/matrix-client-server/src/rooms/room_information/room_aliases.ts
+++ packages/matrix-client-server/src/rooms/room_information/room_aliases.ts
@@ -8,38 +8,39 @@ export const getRoomAliases = (
   return (req, res) => {
     const roomId: string = (req as Request).params.roomId
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
-    clientServer.authenticate(req, res, async (data, id) => {
-      const userId = data.sub
+    clientServer.authenticate(req, res, async (token) => {
+      const userId = token.sub
 
       try {
-        // Check if the user has the rights to access the room aliases
-        let accessible = false
-
+        // Check the history visibility of the room
         const historyResponse = await clientServer.matrixDb.get(
           'room_stats_state',
           ['history_visibility'],
           { room_id: roomId }
         )
+
         if (historyResponse.length === 0) {
           send(res, 400, errMsg('invalidParam', 'Invalid room id'))
-          throw new Error('Invalid room id')
+          return
         }
 
-        if (historyResponse[0]?.history_visibility === 'world_readable') {
+        let accessible = false
+        if (historyResponse[0].history_visibility === 'world_readable') {
           accessible = true
         } else {
+          // Check if the user is a member of the room
           const membershipResponse = await clientServer.matrixDb.get(
-            'local_current_membership',
-            ['membership'],
+            'room_memberships',
+            ['event_id'],
             {
               room_id: roomId,
-              user_id: userId
+              user_id: userId,
+              membership: 'join',
+              forgotten: 0
             }
           )
-          if (
-            membershipResponse.length > 0 &&
-            membershipResponse[0].membership === 'join'
-          ) {
+
+          if (membershipResponse.length > 0) {
             accessible = true
           }
         }
@@ -53,20 +54,21 @@ export const getRoomAliases = (
               'The user is not permitted to retrieve the list of local aliases for the room'
             )
           )
-          throw new Error(
-            'The user is not permitted to retrieve the list of local aliases for the room'
-          )
         } else {
+          // Fetch the room aliases
           const aliasRows = await clientServer.matrixDb.get(
             'room_aliases',
             ['room_alias'],
             { room_id: roomId }
           )
           const roomAliases = aliasRows.map((row) => row.room_alias)
-          send(res, 200, { room_aliases: roomAliases })
+          send(res, 200, { aliases: roomAliases })
         }
-      } catch (error) {
-        clientServer.logger.error('Error retrieving room aliases:', error)
+      } catch (e) {
+        /* istanbul ignore next */
+        clientServer.logger.error(e)
+        /* istanbul ignore next */
+        send(res, 500, errMsg('unknown', e as string))
       }
     })
   }
