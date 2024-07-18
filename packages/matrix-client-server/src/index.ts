ad703158f0c5c127f9c8fce0c2f78cf7b38ebf1d
BichraiX <160038319+BichraiX@users.noreply.github.com>
2024-07-18 11:28:34 +0400

    feat : added /bind endpoint. Update register function to handle guest upgrade to user account. Changed "tokenContent" to "TokenContent" from authentication for clarification and to distinguish it from matrix-identity-server.

--- packages/matrix-client-server/src/index.ts
+++ packages/matrix-client-server/src/index.ts
@@ -61,6 +61,7 @@ import bind from './account/3pid/bind'
 import refresh from './refresh'
 import bind from './account/3pid/bind'
 import add from './account/3pid/add'
+import bind from './account/3pid/bind'
 
 const tables = {
   ui_auth_sessions: 'session_id TEXT NOT NULL, stage_type TEXT NOT NULL'
