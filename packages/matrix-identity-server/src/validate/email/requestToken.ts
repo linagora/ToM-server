a9686254104deb9d2ddc95f8caf05f7600656ef9
BichraiX <160038319+BichraiX@users.noreply.github.com>
2024-07-22 07:51:38 +0100

    fix : added a constant for maximum send attempts in requesttoken

--- packages/matrix-identity-server/src/validate/email/requestToken.ts
+++ packages/matrix-identity-server/src/validate/email/requestToken.ts
@@ -29,6 +29,7 @@ const schema = {
 
 const clientSecretRe = /^[0-9a-zA-Z.=_-]{6,255}$/
 const validEmailRe = /^\w[+.-\w]*\w@\w[.-\w]*\w\.\w{2,6}$/
+const maxAttemps = 1000000000
 
 const preConfigureTemplate = (
   template: string,
@@ -163,7 +164,7 @@ const RequestToken = <T extends string = never>(
             send(res, 400, errMsg('invalidParam', 'invalid next_link'))
           } else if (
             typeof sendAttempt !== 'number' ||
-            sendAttempt > 1000000000
+            sendAttempt > maxAttemps
           ) {
             send(res, 400, errMsg('invalidParam', 'Invalid send attempt'))
           } else {
