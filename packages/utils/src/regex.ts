8f1dfef559712b9c639801c23ed2f2c27ec07571
h1pp0X <hippolyte.wallaert@polytechnique.edu>
2024-07-25 10:07:24 +0400

    fix : fixed type syntax

--- packages/utils/src/regex.ts
+++ packages/utils/src/regex.ts
@@ -8,7 +8,7 @@ const sidRegex: RegExp = /^[0-9a-zA-Z.=_-]{1,255}$/
 const countryRegex: RegExp = /^[A-Z]{2}$/ // ISO 3166-1 alpha-2 as per the spec : https://spec.matrix.org/v1.11/client-server-api/#post_matrixclientv3registermsisdnrequesttoken
 const phoneNumberRegex: RegExp = /^[1-9]\d{1,14}$/
 const emailRegex: RegExp = /^\w[+.-\w]*\w@\w[.-\w]*\w\.\w{2,6}$/
-const roomAliasRegex = /^#[a-zA-Z0-9_\-=.+]+:[a-zA-Z0-9\-.]+$/ // From : https://spec.matrix.org/v1.11/#room-structure
+const roomAliasRegex: RegExp = /^#[a-zA-Z0-9_\-=.+]+:[a-zA-Z0-9\-.]+$/ // From : https://spec.matrix.org/v1.11/#room-structure
 
 export const isClientSecretValid = (clientSecret: string): boolean =>
   clientSecretRegex.test(clientSecret)
