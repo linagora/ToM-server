925d658a14728adba96612c3b82fa07f0e4117d7
h1pp0X <hippolyte.wallaert@polytechnique.edu>
2024-07-26 12:52:00 +0400

    fix : using correct method to get from the db

--- packages/matrix-identity-server/src/keyManagement/getPubkey.ts
+++ packages/matrix-identity-server/src/keyManagement/getPubkey.ts
@@ -3,19 +3,19 @@ import type IdentityServerDB from '../db'
 import { errMsg, send, type expressAppHandler } from '@twake/utils'
 
 const getPubkey = <T extends string = never>(
-  idServer: IdentityServerDB<T>
+  idServerDB: IdentityServerDB<T>
 ): expressAppHandler => {
   return (req, res) => {
     const _keyID: string = (req as Request).params.keyId
 
-    idServer.db
+    idServerDB
       .get('shortTermKeypairs', ['public'], { keyID: _keyID })
       // eslint-disable-next-line @typescript-eslint/promise-function-async
       .then((rows) => {
         if (rows.length === 1) {
           send(res, 200, { public_key: rows[0].public })
         } else {
-          return idServer.db
+          return idServerDB
             .get('longTermKeypairs', ['public'], { keyID: _keyID })
             .then((rows) => {
               if (rows.length === 0) {
