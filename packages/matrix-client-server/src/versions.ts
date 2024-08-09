5ba806016ff1ae1fea38ff9110c138362dddb5ba
Mathixx <147631380+Mathixx@users.noreply.github.com>
2024-08-09 11:49:23 +0400

    feat: added version API

---
+++ packages/matrix-client-server/src/versions.ts
@@ -0,0 +1,21 @@
+import { send, type expressAppHandler } from '@twake/utils'
+
+// TODO: fix supported versions
+export const versions = [
+  // 'r0.1.0',
+  // 'r0.2.0',
+  // 'r0.2.1',
+  // 'r0.3.0',
+  'v1.1',
+  'v1.2',
+  'v1.3',
+  'v1.4',
+  'v1.5',
+  'v1.6'
+]
+
+const getVersions: expressAppHandler = (req, res) => {
+  send(res, 200, { versions })
+}
+
+export default getVersions
