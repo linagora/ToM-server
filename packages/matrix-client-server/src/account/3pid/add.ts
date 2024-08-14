9b915ec760b0e340764ff30b744b98546079d5f3
BichraiX <160038319+BichraiX@users.noreply.github.com>
2024-08-14 11:08:04 +0400

    fix : filled empty catch statements

--- packages/matrix-client-server/src/account/3pid/add.ts
+++ packages/matrix-client-server/src/account/3pid/add.ts
@@ -10,6 +10,7 @@ import {
 import { type AuthenticationData } from '../../types'
 import type MatrixClientServer from '../..'
 import { validateUserWithUIAuthentication } from '../../utils/userInteractiveAuthentication'
+import { isAdmin } from '../../utils/utils'
 
 interface RequestBody {
   auth: AuthenticationData
@@ -39,94 +40,135 @@ const add = (clientServer: MatrixClientServer): expressAppHandler => {
         'add a 3pid to a user account',
         data,
         (obj, userId) => {
-          validateParameters(res, schema, obj, clientServer.logger, (obj) => {
-            if (!isClientSecretValid((obj as RequestBody).client_secret)) {
-              send(
-                res,
-                400,
-                errMsg('invalidParam', 'Invalid client_secret'),
-                clientServer.logger
-              )
-              return
-            }
-            if (!isSidValid((obj as RequestBody).sid)) {
-              send(
-                res,
-                400,
-                errMsg('invalidParam', 'Invalid session ID'),
-                clientServer.logger
-              )
-              return
-            }
-            const body = obj as RequestBody
-            clientServer.matrixDb
-              .get(
-                'threepid_validation_session',
-                ['address', 'medium', 'validated_at'],
-                {
-                  // Get the address from the validation session. This API has to be called after /requestToken, else it will send error 400
-                  client_secret: body.client_secret,
-                  session_id: body.sid
-                }
-              )
-              .then((sessionRows) => {
-                if (sessionRows.length === 0) {
-                  send(res, 400, errMsg('noValidSession'), clientServer.logger)
-                  return
-                }
-                // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-                if (!sessionRows[0].validated_at) {
-                  send(
-                    res,
-                    400,
-                    errMsg('sessionNotValidated'),
-                    clientServer.logger
-                  )
-                  return
-                }
-                clientServer.matrixDb
-                  .get('user_threepids', ['user_id'], {
-                    address: sessionRows[0].address
-                  })
-                  .then((rows) => {
-                    if (rows.length > 0) {
-                      send(
-                        res,
-                        400,
-                        errMsg('threepidInUse'),
-                        clientServer.logger
+          validateParameters(
+            res,
+            schema,
+            obj,
+            clientServer.logger,
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            async (obj) => {
+              if (!isClientSecretValid((obj as RequestBody).client_secret)) {
+                send(
+                  res,
+                  400,
+                  errMsg('invalidParam', 'Invalid client_secret'),
+                  clientServer.logger
+                )
+                return
+              }
+              if (!isSidValid((obj as RequestBody).sid)) {
+                send(
+                  res,
+                  400,
+                  errMsg('invalidParam', 'Invalid session ID'),
+                  clientServer.logger
+                )
+                return
+              }
+              const body = obj as RequestBody
+              const byAdmin = await isAdmin(clientServer, userId as string)
+              const allowed =
+                clientServer.conf.capabilities.enable_set_avatar_url ?? true
+              clientServer.matrixDb
+                .get(
+                  'threepid_validation_session',
+                  ['address', 'medium', 'validated_at'],
+                  {
+                    // Get the address from the validation session. This API has to be called after /requestToken, else it will send error 400
+                    client_secret: body.client_secret,
+                    session_id: body.sid
+                  }
+                )
+                .then((sessionRows) => {
+                  if (sessionRows.length === 0) {
+                    send(
+                      res,
+                      400,
+                      errMsg('noValidSession'),
+                      clientServer.logger
+                    )
+                    return
+                  }
+                  if (
+                    sessionRows[0].validated_at === null ||
+                    sessionRows[0].validated_at === undefined
+                  ) {
+                    send(
+                      res,
+                      400,
+                      errMsg('sessionNotValidated'),
+                      clientServer.logger
+                    )
+                    return
+                  }
+                  clientServer.matrixDb
+                    .get('user_threepids', ['user_id'], {
+                      address: sessionRows[0].address
+                    })
+                    .then((rows) => {
+                      if (rows.length > 0) {
+                        send(
+                          res,
+                          400,
+                          errMsg('threepidInUse'),
+                          clientServer.logger
+                        )
+                      } else if (!byAdmin && !allowed) {
+                        send(
+                          res,
+                          403,
+                          errMsg(
+                            'forbidden',
+                            'Cannot add 3pid as not allowed by server'
+                          ),
+                          clientServer.logger
+                        )
+                      } else {
+                        clientServer.matrixDb
+                          .insert('user_threepids', {
+                            user_id: userId as string,
+                            address: sessionRows[0].address as string,
+                            medium: sessionRows[0].medium as string,
+                            validated_at: sessionRows[0].validated_at as number,
+                            added_at: epoch()
+                          })
+                          .then(() => {
+                            send(res, 200, {}, clientServer.logger)
+                          })
+                          .catch((e) => {
+                            // istanbul ignore next
+                            clientServer.logger.error(
+                              'Error while inserting user_threepids'
+                            )
+                            // istanbul ignore next
+                            send(
+                              res,
+                              400,
+                              errMsg('unknown', e),
+                              clientServer.logger
+                            )
+                          })
+                      }
+                    })
+                    .catch((e) => {
+                      // istanbul ignore next
+                      clientServer.logger.error(
+                        'Error while getting user_threepids'
                       )
-                    } else {
-                      clientServer.matrixDb
-                        .insert('user_threepids', {
-                          user_id: userId as string,
-                          address: sessionRows[0].address as string,
-                          medium: sessionRows[0].medium as string,
-                          validated_at: sessionRows[0].validated_at as number,
-                          added_at: epoch()
-                        })
-                        .then(() => {
-                          send(res, 200, {}, clientServer.logger)
-                        })
-                        .catch((e) => {
-                          // istanbul ignore next
-                          clientServer.logger.error(
-                            'Error while inserting user_threepids'
-                          )
-                          // istanbul ignore next
-                          send(
-                            res,
-                            400,
-                            errMsg('unknown', e),
-                            clientServer.logger
-                          )
-                        })
-                    }
-                  })
-                  .catch((e) => {})
-              })
-              .catch((e) => {})
-          })
+                      // istanbul ignore next
+                      send(res, 500, errMsg('unknown', e), clientServer.logger)
+                    })
+                })
+                .catch((e) => {
+                  // istanbul ignore next
+                  clientServer.logger.error(
+                    'Error while getting threepid_validation_session'
+                  )
+                  // istanbul ignore next
+                  send(res, 500, errMsg('unknown', e), clientServer.logger)
+                })
+            }
+          )
         }
       )
     })
