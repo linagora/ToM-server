ef46a34bd76fc790c1a52f6dac3e525fa4e65e68
BichraiX <160038319+BichraiX@users.noreply.github.com>
2024-08-23 10:51:01 +0400

    fix : removed useless console logs

--- packages/matrix-client-server/src/index.test.ts
+++ packages/matrix-client-server/src/index.test.ts
@@ -2066,7 +2066,6 @@ describe('Use configuration file', () => {
               session
             }
           })
-        console.log('body : ', response.body)
         expect(response.status).toBe(200)
         const remainingMessages = await clientServer.matrixDb.get(
           'device_inbox',
