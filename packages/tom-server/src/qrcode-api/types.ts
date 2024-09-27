d057440d4d1b379f9d2ef978fa328fb03aa2584d
Khaled FERJANI <kferjani@linagora.com>
2024-09-27 11:54:49 +0200

    🏷️ chore: add QRCode API types

---
+++ packages/tom-server/src/qrcode-api/types.ts
@@ -0,0 +1,10 @@
+import { type Response, type NextFunction } from 'express'
+import { type AuthRequest } from '../types'
+
+export interface IQRCodeApiController {
+  get: (req: AuthRequest, res: Response, next: NextFunction) => Promise<void>
+}
+
+export interface IQRCodeService {
+  get: (id: string) => Promise<string | null>
+}
