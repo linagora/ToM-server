5e78d55960cfa3915198b6079318573e9ed41abd
Khaled FERJANI <kferjani@linagora.com>
2024-10-04 17:29:26 +0200

    🏷️ chore: update qrcode API related types

--- packages/tom-server/src/qrcode-api/types.ts
+++ packages/tom-server/src/qrcode-api/types.ts
@@ -6,5 +6,30 @@ export interface IQRCodeApiController {
 }
 
 export interface IQRCodeService {
-  get: (id: string) => Promise<string | null>
+  getImage: (token: string) => Promise<string | null>
+}
+
+export interface IQRCodeTokenService {
+  getAccessToken: (loginToken: string) => Promise<string | null>
+}
+
+export interface TokenLoginPayload {
+  initial_device_display_name: string
+  token: string
+  type: string
+}
+
+export interface GenericResponse {
+  errcode?: string
+  error?: string
+}
+
+export interface TokenLoginResponse extends GenericResponse {
+  access_token: string
+  device_id: string
+  expires_in_ms: number
+  home_server: string
+  refresh_token: string
+  user_id: string
+  well_known?: object
 }
