9a3d89c5c3d91315014fa3bd30ca05c717087cc8
Khaled FERJANI <kferjani@linagora.com>
2024-12-11 13:52:55 +0100

    🧪 chore: fix tests

--- packages/tom-server/src/metrics-api/tests/controller.test.ts
+++ packages/tom-server/src/metrics-api/tests/controller.test.ts
@@ -116,6 +116,14 @@ describe('the mectrics API controller', () => {
   describe('the getActivityStats handler', () => {
     it('should try to fetch the user activity metrics', async () => {
       dbMock.getAll.mockResolvedValue([TODAY_USER])
+      dbMock.get.mockImplementation(async () => {
+        return await Promise.resolve([
+          {
+            user_id: TODAY_USER.name,
+            last_seen: TODAY_USER.last_seen_ts
+          }
+        ])
+      })
 
       const response = await supertest(app).get(`${PATH}/activity`).send()
 
@@ -131,7 +139,30 @@ describe('the mectrics API controller', () => {
 
     it('should calculate daily active users correctly', async () => {
       dbMock.getAll.mockResolvedValue([TODAY_USER, PRE_TODAY_USER])
-
+      dbMock.get.mockImplementation(
+        async (
+          _table: string,
+          _fields: string[],
+          filters: Record<string, string | number>
+        ) => {
+          switch (filters.user_id) {
+            case TODAY_USER.name:
+              return await Promise.resolve([
+                {
+                  user_id: TODAY_USER.name,
+                  last_seen: TODAY_USER.last_seen_ts
+                }
+              ])
+            case PRE_TODAY_USER.name:
+              return await Promise.resolve([
+                {
+                  user_id: PRE_TODAY_USER.name,
+                  last_seen: PRE_TODAY_USER.last_seen_ts
+                }
+              ])
+          }
+        }
+      )
       const response = await supertest(app).get(`${PATH}/activity`).send()
 
       expect(response.status).toBe(200)
@@ -146,6 +177,30 @@ describe('the mectrics API controller', () => {
 
     it('should calculate weekly active users correctly', async () => {
       dbMock.getAll.mockResolvedValue([TODAY_USER, PRE_WEEK_USER])
+      dbMock.get.mockImplementation(
+        async (
+          _table: string,
+          _fields: string[],
+          filters: Record<string, string | number>
+        ) => {
+          switch (filters.user_id) {
+            case TODAY_USER.name:
+              return await Promise.resolve([
+                {
+                  user_id: TODAY_USER.name,
+                  last_seen: TODAY_USER.last_seen_ts
+                }
+              ])
+            case PRE_WEEK_USER.name:
+              return await Promise.resolve([
+                {
+                  user_id: PRE_WEEK_USER.name,
+                  last_seen: PRE_WEEK_USER.last_seen_ts
+                }
+              ])
+          }
+        }
+      )
 
       const response = await supertest(app).get(`${PATH}/activity`).send()
 
@@ -161,6 +216,30 @@ describe('the mectrics API controller', () => {
 
     it('should calculate monthly active users correctly', async () => {
       dbMock.getAll.mockResolvedValue([PRE_WEEK_USER, PRE_MONTH_USER])
+      dbMock.get.mockImplementation(
+        async (
+          _table: string,
+          _fields: string[],
+          filters: Record<string, string | number>
+        ) => {
+          switch (filters.user_id) {
+            case PRE_WEEK_USER.name:
+              return await Promise.resolve([
+                {
+                  user_id: PRE_WEEK_USER.name,
+                  last_seen: PRE_WEEK_USER.last_seen_ts
+                }
+              ])
+            case PRE_MONTH_USER.name:
+              return await Promise.resolve([
+                {
+                  user_id: PRE_MONTH_USER.name,
+                  last_seen: PRE_MONTH_USER.last_seen_ts
+                }
+              ])
+          }
+        }
+      )
 
       const response = await supertest(app).get(`${PATH}/activity`).send()
 
@@ -187,7 +266,24 @@ describe('the mectrics API controller', () => {
     it('should try to fetch the message stats', async () => {
       dbMock.getAll.mockResolvedValue([TODAY_USER])
 
-      dbMock.get.mockResolvedValue(messages)
+      dbMock.get.mockImplementation(
+        async (
+          table: string,
+          _fields: string[],
+          filters: Record<string, string | number>
+        ) => {
+          if (table === 'events') {
+            return messages
+          }
+
+          return await Promise.resolve([
+            {
+              user_id: TODAY_USER.name,
+              last_seen: TODAY_USER.last_seen_ts
+            }
+          ])
+        }
+      )
 
       const response = await supertest(app).get(`${PATH}/messages`).send()
 
