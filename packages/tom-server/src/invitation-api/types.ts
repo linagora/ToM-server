1ec5e18bf5aa58f660da5ad3c3be8d3dd1ce0849
Khaled FERJANI <kferjani@linagora.com>
2024-12-19 12:48:35 +0100

    🏷️ chore: added invitation related types

---
+++ packages/tom-server/src/invitation-api/types.ts
@@ -0,0 +1,28 @@
+export interface IInvitationService {
+  invite: (payload: invitationPayload) => Promise<void>
+  accept: (token: string) => Promise<void>
+  list: (userId: string) => Promise<Invitation[]>
+  generateLink: (payload: invitationPayload) => Promise<string>
+}
+
+export type medium = 'email' | 'phone'
+
+export interface Invitation {
+  id: string
+  sender: string
+  recepient: string
+  medium: medium
+  expiration: number
+  accessed: boolean
+}
+
+export interface InvitationRequestPayload {
+  contact: string
+  medium: medium
+}
+
+export interface invitationPayload {
+  sender: string
+  recepient: string
+  medium: medium
+}
