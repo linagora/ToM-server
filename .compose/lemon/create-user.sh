edaf273ec428c3809bdb2cc87e573f3ce08e9829
Pierre 'McFly' Marty <pmarty@linagora.com>
2025-01-27 14:43:58 +0200

    chore(lemon): add more default users
    	added:
    	  - dwho / dwho
    	  - rtyler / rtyler
    	  - jbinks / jbinks
    
    Signed-off-by: Pierre 'McFly' Marty <pmarty@linagora.com>

--- .compose/lemon/create-user.sh
+++ .compose/lemon/create-user.sh
@@ -3,11 +3,13 @@
 # License           : AGPL-3.0-or-later
 # Author            : Pierre 'McFly' Marty <pmarty@linagora.com>
 # Date              : 14.01.2025
-# Last Modified Date: 14.01.2025
+# Last Modified Date: 27.01.2025
 # Last Modified By  : Pierre 'McFly' Marty <pmarty@linagora.com>
 
 readonly nickname="${1:-dwho}"
 readonly givenname="${2:-Doctor Who}"
 readonly password="${3:-dwho}"
 
+echo -e "Adding user: ${givenname} (${nickname})\n\tpass: ${password}"
 sqlite3 lemon.db "insert into users values ('${nickname}','${givenname}','${nickname}@docker.localhost','${password}')"
+[[ "$?" == '0' ]] && echo "Done."
--- .compose/lemon/init-db.sh
+++ .compose/lemon/init-db.sh
@@ -3,8 +3,14 @@
 # License           : AGPL-3.0-or-later
 # Author            : Pierre 'McFly' Marty <pmarty@linagora.com>
 # Date              : 14.01.2025
-# Last Modified Date: 14.01.2025
+# Last Modified Date: 27.01.2025
 # Last Modified By  : Pierre 'McFly' Marty <pmarty@linagora.com>
 
+echo "Inflating Auth DB."
 sqlite3 lemon.db "Create table if not exists users (uid text,name text,mail text,password text);"
-./create-user.sh
+[[ "$?" == '0' ]] && echo "Done."
+
+#                # nickname       # Full Name       # password
+./create-user.sh 'dwho'           'Doctor Who'      'dwho'
+./create-user.sh 'rtyler'         'R. Tyler'        'rtyler'
+./create-user.sh 'jbinks'         'Jar Jar Binks'   'jbinks'
