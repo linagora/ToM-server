a5e58249c2700f5b0df6a88b690263f52e0385d7
Khaled FERJANI <kferjani@linagora.com>
2025-02-18 11:52:50 +0100

    🧪 chore: added utils tests

---
+++ packages/matrix-identity-server/src/utils.test.ts
@@ -0,0 +1,11 @@
+import { getServerNameFromMatrixId } from './utils'
+
+describe('the getServerNameFromMatrixId helper', () => {
+  it('should extract the server name correctly', () => {
+    const firstServer = getServerNameFromMatrixId('@test:example.com')
+    const secondServer = getServerNameFromMatrixId('@support:linagora.com')
+
+    expect(firstServer).toEqual('example.com')
+    expect(secondServer).toEqual('linagora.com')
+  })
+})
