46249fea32749ae400dfa5584b5d5a23815431af
Khaled FERJANI <kferjani@linagora.com>
2025-02-24 16:52:23 +0100

    🎨 feat: update addressbook service

--- packages/tom-server/src/addressbook-api/services/index.ts
+++ packages/tom-server/src/addressbook-api/services/index.ts
@@ -55,50 +55,12 @@ export class AddressbookService implements IAddressbookService {
   }
 
   /**
-   * Create a new addressbook with initial contact list
-   *
-   * @param {string} owner - The owner of the addressbook
-   * @param {ContactCreationPayload[]} contacts - The initial contact list
-   * @returns {Promise<AddressbookListResponse | undefined>}
-   */
-  public create = async (
-    owner: string,
-    contacts?: ContactCreationPayload[]
-  ): Promise<AddressbookListResponse | undefined> => {
-    try {
-      const id = uuidv7()
-      const addressbook = await this.db.insert('addressbooks', {
-        owner,
-        id
-      })
-
-      if (!addressbook || !addressbook.length) {
-        throw new Error()
-      }
-
-      if (contacts) {
-        for (const contact of contacts) {
-          await this._insertContact(contact, id)
-        }
-      }
-
-      return {
-        id,
-        owner,
-        contacts: await this._listAddressbookContacts(id)
-      }
-    } catch (error) {
-      this.logger.error('Failed to create addressbook', { error })
-    }
-  }
-
-  /**
    * Deletes an addressbook
    *
    * @param {string} owner - The id of the addressbook
    * @returns {Promise<void>}
    */
-  delete = async (owner: string): Promise<void> => {
+  public delete = async (owner: string): Promise<void> => {
     try {
       const userAddressbook = (await this.db.get('addressbooks', ['id'], {
         owner
@@ -118,24 +80,76 @@ export class AddressbookService implements IAddressbookService {
   }
 
   /**
+   * Get a contact by id
+   *
+   * @param {string} contactId - the contact id
+   */
+  public getContact = async (
+    contactId: string
+  ): Promise<Contact | undefined> => {
+    try {
+      const queryResult = (await this.db.get('contacts', ['*'], {
+        id: contactId
+      })) as unknown as Contact[]
+
+      if (!queryResult || !queryResult.length) {
+        throw new Error('Contact not found')
+      }
+
+      return queryResult[0]
+    } catch (error) {
+      this.logger.error('Failed to get contact', { error })
+    }
+  }
+
+  /**
    * Add a contact to an addressbook
    *
-   * @param {string} addressbookId - The id of the addressbook
+   * @param {string} owner - The id of the addressbook
    * @param {ContactCreationPayload} contact - The contact to add
    * @returns {Promise<Contact>}
    */
-  addContact = async (
-    addressbookId: string,
-    contact: ContactCreationPayload
-  ): Promise<Contact | undefined> => {
+  public addContacts = async (
+    owner: string,
+    contacts?: ContactCreationPayload[]
+  ): Promise<AddressbookListResponse | undefined> => {
     try {
-      const created = await this._insertContact(contact, addressbookId)
+      let createdContacts: Contact[] = []
+      let addressbook = await this._getUserAddressBook(owner)
 
-      if (!created) {
-        throw new Error()
+      if (!addressbook) {
+        this.logger.info('Addressbook not found, creating one')
+
+        addressbook = await this._createUserAddressBook(owner)
+
+        if (!addressbook) {
+          throw new Error('Failed to create addressbook')
+        }
       }
 
-      return created
+      const { id } = addressbook
+
+      if (contacts) {
+        for (const contact of contacts) {
+          const exists = await this._checkIfContactExists(contact, id)
+
+          if (!exists) {
+            const createdContact = await this._insertContact(contact, id)
+
+            if (createdContact && createdContact.id) {
+              createdContacts.push(createdContact)
+            } else {
+              this.logger.error(`Failed to create contact`)
+            }
+          }
+        }
+      }
+
+      return {
+        id,
+        owner,
+        contacts: createdContacts
+      }
     } catch (error) {
       this.logger.error('Failed to add contact', { error })
     }
@@ -148,14 +162,16 @@ export class AddressbookService implements IAddressbookService {
    * @param {ContactUpdatePayload} contact - The contact to update
    * @returns {Promise<Contact>}
    */
-  updateContact = async (
+  public updateContact = async (
     id: string,
     contact: ContactUpdatePayload
   ): Promise<Contact | undefined> => {
     try {
+      const { mxid, ...payload } = contact
+
       const updatedContact = (await this.db.update(
         'contacts',
-        { ...contact, id },
+        payload,
         'id',
         id
       )) as unknown as Contact[]
@@ -176,7 +192,7 @@ export class AddressbookService implements IAddressbookService {
    * @param {string} contactId - The id of the contact
    * @returns {Promise<void>}
    */
-  deleteContact = async (contactId: string): Promise<void> => {
+  public deleteContact = async (contactId: string): Promise<void> => {
     try {
       await this.db.deleteEqual('contacts', 'id', contactId)
     } catch (error) {
@@ -196,8 +212,10 @@ export class AddressbookService implements IAddressbookService {
     addressbookId: string
   ): Promise<Contact | undefined> => {
     try {
+      const id = uuidv7()
       const created = (await this.db.insert('contacts', {
         ...contact,
+        id,
         addressbook_id: addressbookId
       })) as unknown as Contact[]
 
@@ -215,19 +233,93 @@ export class AddressbookService implements IAddressbookService {
    */
   private _listAddressbookContacts = async (id: string): Promise<Contact[]> => {
     try {
-      const contacts = (await this.db.get(
-        'contacts',
-        ['id', 'mxid', 'display_name', 'active'],
-        {
-          adressbook_id: id
-        }
-      )) as unknown as Contact[]
+      const contacts = (await this.db.get('contacts', ['*'], {
+        addressbook_id: id
+      })) as unknown as Contact[]
 
       return contacts
     } catch (error) {
       this.logger.error('Failed to list addressbook', { error })
 
       return []
+    }
+  }
+
+  /**
+   * Fetches the user addressbook
+   *
+   * @param {string} owner
+   * @returns {Promise<AddressBook | undefined>}
+   */
+  private _getUserAddressBook = async (
+    owner: string
+  ): Promise<AddressBook | undefined> => {
+    try {
+      const userAddressbook = (await this.db.get('addressbooks', ['*'], {
+        owner
+      })) as unknown as AddressBook[]
+
+      if (!userAddressbook || !userAddressbook.length) {
+        throw new Error('Addressbook not found')
+      }
+
+      return userAddressbook[0]
+    } catch (error) {
+      this.logger.error('Failed to get user addressbook', { error })
+    }
+  }
+
+  /**
+   * Creates a new addressbook for a user
+   *
+   * @param {string} owner
+   * @returns {Promise<AddressBook | undefined>}
+   */
+  private _createUserAddressBook = async (
+    owner: string
+  ): Promise<AddressBook | undefined> => {
+    try {
+      const id = uuidv7()
+      const addressbook = (await this.db.insert('addressbooks', {
+        owner,
+        id
+      })) as unknown as AddressBook[]
+
+      if (!addressbook || !addressbook.length) {
+        throw new Error()
+      }
+
+      return addressbook[0]
+    } catch (error) {
+      this.logger.error('Failed to create user addressbook', { error })
+    }
+  }
+
+  /**
+   * Checks if a contact already exists within addressbook
+   *
+   * @param {ContactCreationPayload} contact - The contact to check
+   * @param {string} addressbookId - The addressbook id
+   * @returns {Promise<boolean>}
+   */
+  private _checkIfContactExists = async (
+    contact: ContactCreationPayload,
+    addressbookId: string
+  ): Promise<boolean> => {
+    try {
+      const { mxid, display_name } = contact
+
+      const contactExists = (await this.db.get('contacts', ['*'], {
+        mxid,
+        display_name,
+        addressbook_id: addressbookId
+      })) as unknown as Contact[]
+
+      return contactExists.length > 0
+    } catch (error) {
+      this.logger.error('Failed to check if contact exists', { error })
+
+      return false
     }
   }
 }
