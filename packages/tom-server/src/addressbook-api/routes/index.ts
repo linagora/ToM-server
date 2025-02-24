641173a8bffa30271bc3a7b85d5d3a0ec46877e6
Khaled FERJANI <kferjani@linagora.com>
2025-02-24 16:55:10 +0100

    ✨ feat: add addressbook API router

--- packages/tom-server/src/addressbook-api/routes/index.ts
+++ packages/tom-server/src/addressbook-api/routes/index.ts
@@ -3,21 +3,58 @@ import {
   type Config as LoggerConfig,
   type TwakeLogger
 } from '@twake/logger'
-import type IdServer from '../../identity-server'
-import type { Config } from '../../types'
+import type { AuthenticationFunction, Config, TwakeDB } from '../../types'
 import { Router } from 'express'
 import authMiddleware from '../../utils/middlewares/auth.middleware'
+import { AddressbookApiController } from '../controllers'
+import { AddressBookApiMiddleware } from '../middlewares'
 
 export const PATH = '/_twake/addressbook'
 
 export default (
-  idServer: IdServer,
   config: Config,
+  db: TwakeDB,
+  authenticator: AuthenticationFunction,
   defaultLogger?: TwakeLogger
 ): Router => {
   const logger = defaultLogger ?? getLogger(config as unknown as LoggerConfig)
   const router = Router()
-  const authenticator = authMiddleware(idServer.authenticate, logger)
+  const authenticate = authMiddleware(authenticator, logger)
+  const controller = new AddressbookApiController(db, logger)
+  const middleware = new AddressBookApiMiddleware(db, logger)
+
+  router.get(PATH, authenticate, controller.listAddressbook)
+
+  router.delete(PATH, authenticate, controller.deleteAddressbook)
+
+  router.post(
+    PATH,
+    authenticate,
+    middleware.validateContactsCreation,
+    controller.addContacts
+  )
+
+  router.get(
+    `${PATH}/:id`,
+    authenticate,
+    middleware.checkContactOwnership,
+    controller.fetchContact
+  )
+
+  router.put(
+    `${PATH}/:id`,
+    authenticate,
+    middleware.checkContactOwnership,
+    middleware.validateContactUpdate,
+    controller.updateContact
+  )
+
+  router.delete(
+    `${PATH}/:id`,
+    authenticate,
+    middleware.checkContactOwnership,
+    controller.deleteContact
+  )
 
   return router
 }
