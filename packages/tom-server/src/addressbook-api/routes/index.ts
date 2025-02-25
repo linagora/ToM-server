8abe39e423199598b5bbcea8e05899b519e8e9df
Khaled FERJANI <kferjani@linagora.com>
2025-02-25 10:31:47 +0100

    📝 chore: add swagger docs

--- packages/tom-server/src/addressbook-api/routes/index.ts
+++ packages/tom-server/src/addressbook-api/routes/index.ts
@@ -23,10 +23,133 @@ export default (
   const controller = new AddressbookApiController(db, logger)
   const middleware = new AddressBookApiMiddleware(db, logger)
 
+  /**
+   * @openapi
+   * components:
+   *  schemas:
+   *    Addressbook:
+   *      type: object
+   *      properties:
+   *        id:
+   *          type: string
+   *        owner:
+   *          type: string
+   *        contacts:
+   *          type: array
+   *          items:
+   *            $ref: '#/components/schemas/Contact'
+   *    Contact:
+   *      type: object
+   *      properties:
+   *        id:
+   *          type: string
+   *        mxid:
+   *          type: string
+   *        display_name:
+   *          type: string
+   *        active:
+   *          type: boolean
+   *        addressbook_id:
+   *          type: string
+   *    ContactCreationPayload:
+   *      type: object
+   *      properties:
+   *        mxid:
+   *          type: string
+   *        display_name:
+   *          type: string
+   *        active:
+   *          type: boolean
+   *    ContactUpdatePayload:
+   *      type: object
+   *      properties:
+   *        display_name:
+   *          type: string
+   *        active:
+   *          type: boolean
+   */
+
+  /**
+   * @openapi
+   * /_twake/addressbook:
+   *  get:
+   *   tags:
+   *   - Addressbook
+   *   description: List all contacts in the addressbook
+   *   responses:
+   *    200:
+   *      description: List of contacts
+   *      content:
+   *        application/json:
+   *          schema:
+   *            type: array
+   *            items:
+   *              $ref: '#/components/schemas/Addressbook'
+   *    401:
+   *      description: Unauthorized
+   *    429:
+   *      description: Too many requests
+   *    500:
+   *      description: Internal server error
+   */
   router.get(PATH, authenticate, controller.listAddressbook)
 
+  /**
+   * @openapi
+   * /_twake/addressbook:
+   *  delete:
+   *   tags:
+   *   - Addressbook
+   *   description: Delete the addressbook
+   *   responses:
+   *    200:
+   *      description: Addressbook deleted
+   *    401:
+   *      description: Unauthorized
+   *    429:
+   *      description: Too many requests
+   *    500:
+   *      description: Internal server error
+   */
   router.delete(PATH, authenticate, controller.deleteAddressbook)
 
+  /**
+   * @openapi
+   * /_twake/addressbook/contacts:
+   *  post:
+   *   tags:
+   *   - Addressbook
+   *   description: Add contacts to the addressbook
+   *   requestBody:
+   *    description: The contacts to add
+   *    required: true
+   *    content:
+   *      application/json:
+   *        schema:
+   *          type: object
+   *          properties:
+   *            contacts:
+   *              type: array
+   *              items:
+   *                $ref: '#/components/schemas/ContactCreationPayload'
+   *   responses:
+   *    200:
+   *      description: List of contacts
+   *      content:
+   *        application/json:
+   *          schema:
+   *            type: array
+   *            items:
+   *              $ref: '#/components/schemas/Addressbook'
+   *    400:
+   *      description: Bad request
+   *    401:
+   *      description: Unauthorized
+   *    429:
+   *      description: Too many requests
+   *    500:
+   *      description: Internal server error
+   */
   router.post(
     PATH,
     authenticate,
@@ -34,6 +157,29 @@ export default (
     controller.addContacts
   )
 
+  /**
+   * @openapi
+   * /_twake/addressbook/contacts/{id}:
+   *  get:
+   *   tags:
+   *   - Addressbook
+   *   description: Get a contact
+   *   responses:
+   *    200:
+   *      description: Contact
+   *      content:
+   *        application/json:
+   *          schema:
+   *            $ref: '#/components/schemas/Contact'
+   *    401:
+   *      description: Unauthorized
+   *    404:
+   *      description: Contact not found
+   *    429:
+   *      description: Too many requests
+   *    500:
+   *      description: Internal server error
+   */
   router.get(
     `${PATH}/:id`,
     authenticate,
@@ -41,6 +187,38 @@ export default (
     controller.fetchContact
   )
 
+  /**
+   * @openapi
+   * /_twake/addressbook/contacts/{id}:
+   *  put:
+   *   tags:
+   *   - Addressbook
+   *   description: Update a contact
+   *   requestBody:
+   *    description: The contact to update
+   *    required: true
+   *    content:
+   *      application/json:
+   *        schema:
+   *          $ref: '#/components/schemas/ContactUpdatePayload'
+   *   responses:
+   *    200:
+   *      description: Contact
+   *      content:
+   *        application/json:
+   *          schema:
+   *            $ref: '#/components/schemas/Contact'
+   *    400:
+   *      description: Bad request
+   *    401:
+   *      description: Unauthorized
+   *    404:
+   *      description: Contact not found
+   *    429:
+   *      description: Too many requests
+   *    500:
+   *      description: Internal server error
+   */
   router.put(
     `${PATH}/:id`,
     authenticate,
@@ -49,6 +227,25 @@ export default (
     controller.updateContact
   )
 
+  /**
+   * @openapi
+   * /_twake/addressbook/contacts/{id}:
+   *  delete:
+   *   tags:
+   *   - Addressbook
+   *   description: Delete a contact
+   *   responses:
+   *    200:
+   *      description: Contact deleted
+   *    401:
+   *      description: Unauthorized
+   *    404:
+   *      description: Contact not found
+   *    429:
+   *      description: Too many requests
+   *    500:
+   *      description: Internal server error
+   */
   router.delete(
     `${PATH}/:id`,
     authenticate,
