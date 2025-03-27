be3d4401240b3d4127634eb42c7dbb2e168ebfaf
Khaled FERJANI <kferjani@linagora.com>
2025-03-27 11:01:35 +0100

    🏷️ chore(types): update IInvitationService interface

--- packages/tom-server/src/invitation-api/types.ts
+++ packages/tom-server/src/invitation-api/types.ts
@@ -3,6 +3,7 @@ export interface IInvitationService {
   accept: (token: string, authorization: string) => Promise<void>
   list: (userId: string) => Promise<Invitation[]>
   generateLink: (payload: InvitationPayload) => Promise<string>
+  getInvitationStatus: (token: string) => Promise<Invitation>
 }
 
 export type medium = 'email' | 'phone'
