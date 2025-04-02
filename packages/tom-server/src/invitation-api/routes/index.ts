d992cfdf29aa468a1d85e4775b25d7f2ddacda7b
Khaled FERJANI <kferjani@linagora.com>
2025-04-02 14:44:52 +0200

    🎨 chore: update link generation route and swagger docs

--- packages/tom-server/src/invitation-api/routes/index.ts
+++ packages/tom-server/src/invitation-api/routes/index.ts
@@ -86,6 +86,8 @@ export default (
    *      description: Unauthorized
    *    429:
    *      description: Too many requests
+   *    500:
+   *      description: Internal Server Error
    */
   router.post(
     PATH,
@@ -121,14 +123,13 @@ export default (
    *      description: Bad request
    *    401:
    *      description: Unauthorized
-   *    429:
-   *      description: Too many requests
+   *    500:
+   *      description: Internal Server Error
    */
   router.post(
     `${PATH}/generate`,
     authenticate,
     middleware.checkInvitationPayload,
-    middleware.rateLimitInvitations,
     controller.generateInvitationLink
   )
 
