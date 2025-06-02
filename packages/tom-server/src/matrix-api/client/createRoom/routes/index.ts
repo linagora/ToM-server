f07f9330fa73a3561eab48e8ac7e85d7c8ba5bb1
Khaled FERJANI <kferjani@linagora.com>
2025-06-02 17:09:37 +0200

    ✨ feat(createRoom): add createRoom API route

---
+++ packages/tom-server/src/matrix-api/client/createRoom/routes/index.ts
@@ -0,0 +1,27 @@
+import {
+  getLogger,
+  type TwakeLogger,
+  type Config as LoggerConfig
+} from '@twake/logger'
+import type { Config } from '../../../../types'
+import { Router } from 'express'
+import CreateRoomController from '../controllers'
+import CreateRoomMiddleware from '../middlewares'
+
+/**
+ * Create room route
+ *
+ * @param {Config} config
+ * @param {TwakeLogger} defaultLogger
+ * @returns {Router}
+ */
+export default (config: Config, defaultLogger?: TwakeLogger): Router => {
+  const logger = defaultLogger ?? getLogger(config as unknown as LoggerConfig)
+  const router = Router()
+  const controller = new CreateRoomController(config, logger)
+  const middleware = new CreateRoomMiddleware(config, logger)
+
+  router.post('/', middleware.checkPayload, controller.createRoom)
+
+  return router
+}
