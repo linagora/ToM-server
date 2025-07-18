3a1bb756913f2d3e47fc9b699aaab5165a36fe66
MontaGhanmy <monta.ghanmy@gmail.com>
2025-07-18 10:41:51 +0100

    ref: error handling

--- packages/tom-server/src/addressbook-api/services/index.ts
+++ packages/tom-server/src/addressbook-api/services/index.ts
@@ -269,12 +269,11 @@ export class AddressbookService implements IAddressbookService {
     if (userAddressbook == null) {
       this.logger.info('Addressbook not found, creating one')
       userAddressbook = await this._createUserAddressBook(owner)
-    }
-
-    // Throw an error is we couldn't get or create an addressbook
-    if (userAddressbook == null) {
-      this.logger.error('Failed to get or create addressbook')
-      throw new Error('Failed to get or create addressbook')
+      // Throw an error is we couldn't get or create an addressbook
+      if (userAddressbook == null) {
+        this.logger.error('Failed to get or create addressbook')
+        throw new Error('Failed to get or create addressbook')
+      }
     }
 
     return userAddressbook
