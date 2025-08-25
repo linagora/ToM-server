64dcd296e4dc9e5245c5ef4bb17c89eaf47f116a
MontaGhanmy <monta.ghanmy@gmail.com>
2025-08-25 18:14:04 +0100

    fix(config-parser): exclude legacy parser from test coverage

--- packages/config-parser/jest.config.js
+++ packages/config-parser/jest.config.js
@@ -1,5 +1,9 @@
 import jestConfigBase from '../../jest-base.config.js'
 
 export default {
-  ...jestConfigBase
+  ...jestConfigBase,
+  collectCoverageFrom: [
+    './src/**/{!(pg|redis),}.ts',
+    '!./src/utils.ts',   // exclude utils file for legacy code coverage
+  ],
 }
