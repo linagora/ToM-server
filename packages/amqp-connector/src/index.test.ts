b50ac88c1cd47c535c01fd3ae5df3d43e8db15ad
MontaGhanmy <monta.ghanmy@gmail.com>
2025-09-15 09:11:10 +0100

    feat(amqp-connector): updated tests

--- packages/amqp-connector/src/index.test.ts
+++ packages/amqp-connector/src/index.test.ts
@@ -11,9 +11,13 @@ jest.mock('amqplib')
 describe('AMQPConnector', () => {
   const mockConsume = jest.fn()
   const mockAck = jest.fn()
+  const mockAssertExchange = jest.fn(() => Promise.resolve())
   const mockAssertQueue = jest.fn(() => Promise.resolve())
+  const mockBindQueue = jest.fn(() => Promise.resolve())
   const mockChannel = {
+    assertExchange: mockAssertExchange,
     assertQueue: mockAssertQueue,
+    bindQueue: mockBindQueue,
     consume: mockConsume,
     ack: mockAck,
     close: jest.fn(() => Promise.resolve())
@@ -48,34 +52,31 @@ describe('AMQPConnector', () => {
     ).rejects.toThrow(MessageHandlerNotProvidedError)
   })
 
-  it.skip('should connect, assert queue, and call handler when a message is consumed', async () => {
-    let consumerCallback: any
+  it('should connect, assert queue, and call handler when a message is consumed', async () => {
+    const fakeMsg = { content: Buffer.from('hello') }
+    const mockHandler = jest.fn()
+
+    // adjust mockConsume to call the callback with fakeMsg
     mockConsume.mockImplementation((_queue, cb) => {
-      consumerCallback = cb
+      cb(fakeMsg)
       return Promise.resolve()
     })
 
-    const handler = jest.fn()
     const connector = new AMQPConnector()
       .withUrl('amqp://localhost')
       .withQueue('test-queue')
-      .onMessage(handler)
+      .onMessage(mockHandler)
 
-    await connector.build() // <-- now async
+    await connector.build()
 
-    const fakeMsg = { content: Buffer.from('hello') }
-    consumerCallback(fakeMsg)
-
-    expect(amqplib.connect).toHaveBeenCalledWith('amqp://localhost')
-    expect(mockCreateChannel).toHaveBeenCalled()
-    expect(mockAssertQueue).toHaveBeenCalledWith('test-queue', {
-      durable: true
-    })
-    expect(handler).toHaveBeenCalledWith(fakeMsg, mockChannel)
+    expect(mockAssertExchange).toHaveBeenCalled()
+    expect(mockAssertQueue).toHaveBeenCalled()
+    expect(mockBindQueue).toHaveBeenCalled()
+    expect(mockHandler).toHaveBeenCalledWith(fakeMsg, mockChannel)
     expect(mockAck).toHaveBeenCalledWith(fakeMsg)
   })
 
-  it.skip('should ignore null messages without throwing', async () => {
+  it('should ignore null messages without throwing', async () => {
     let consumerCallback: any
     mockConsume.mockImplementation((_queue, cb) => {
       consumerCallback = cb
@@ -95,7 +96,7 @@ describe('AMQPConnector', () => {
     expect(mockAck).not.toHaveBeenCalled()
   })
 
-  it.skip('should close channel and connection on close()', async () => {
+  it('should close channel and connection on close()', async () => {
     const connector = new AMQPConnector()
       .withUrl('amqp://localhost')
       .withQueue('test-queue')
