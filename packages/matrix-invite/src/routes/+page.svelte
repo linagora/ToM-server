1fc969819686e323b75789ac20535804997eaa38
Pierre 'McFly' Marty <pmarty@linagora.com>
2026-01-07 20:23:00 +0100

    Removes Outdated Package: matrix-invite (#312)
    Signed-off-by: Pierre 'McFly' Marty <pmarty@linagora.com>

--- packages/matrix-invite/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Invite from '../components/Invite.svelte';
-	import Footer from '../components/Footer.svelte';
-</script>
-
-<div class="mx-auto py-40 px-4 flex-col flex justify-center items-center h-screen w-full md:w-6/12 lg:w-4/12">
-  <Invite />
-  <Footer />
-</div>
