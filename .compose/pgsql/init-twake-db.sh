50fb8fc153980810e074e8bade51af6314b8c3f5
Pierre 'McFly' Marty <pmarty@linagora.com>
2026-01-28 12:00:02 +0100

    chore: updates Twake Chat + SSO compose example
    Signed-off-by: Pierre 'McFly' Marty <pmarty@linagora.com>

--- .compose/pgsql/init-twake-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-psql -U postgres <<-EOSQL
-  CREATE USER twake PASSWORD 'twake!1';
-  CREATE DATABASE fed TEMPLATE='template0' LOCALE='C' ENCODING='UTF8' OWNER='twake';
-  CREATE DATABASE tom TEMPLATE='template0' LOCALE='C' ENCODING='UTF8' OWNER='twake';
-EOSQL
