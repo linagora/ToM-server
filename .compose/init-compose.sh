8500446e28da97a0e6c25bb7f4a0dcd63debf2ae
Pierre 'McFly' Marty <pmarty@linagora.com>
2026-01-28 12:41:46 +0100

    Docker and CI Minor Improvements (#318)
    Signed-off-by: Pierre 'McFly' Marty <pmarty@linagora.com>

--- .compose/init-compose.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-echo "TZ=$(timedatectl show | grep -Poh '(?<=^Timezone=).*')" | tee ./.compose/.env
-echo "UID=$(id -u)" | tee -a ./.compose/.env
-echo "GID=$(id -g)" | tee -a ./.compose/.env
