dc2d0b43f2387935ed9531295dfde9f0e13a1996
Pierre 'McFly' Marty <pmarty@linagora.com>
2026-04-20 12:40:53 +0200

    build(docker): integrates federation service into the img
    Signed-off-by: Pierre 'McFly' Marty <pmarty@linagora.com>

--- .compose/postgres/init.sql
+++ .compose/postgres/init.sql
@@ -1,19 +1,30 @@
 -- Initialize databases and users for ToM-Server
+CREATE USER twake WITH PASSWORD 'twake_password';
 
 -- Create tom_db database and grant permissions to twake user
-CREATE DATABASE tom_db;
+CREATE DATABASE tom_db
+    OWNER twake
+    ENCODING 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TEMPLATE template0;
 GRANT ALL PRIVILEGES ON DATABASE tom_db TO twake;
 
 -- Create fed_db database and grant permissions to twake user
-CREATE DATABASE fed_db;
+CREATE DATABASE fed_db
+    OWNER twake
+    ENCODING 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TEMPLATE template0;
 GRANT ALL PRIVILEGES ON DATABASE fed_db TO twake;
 
 -- Create synapse database and synapse_user with C collation (required by Synapse)
 CREATE USER synapse_user WITH PASSWORD 'secretpassword';
-CREATE DATABASE synapse 
-  OWNER synapse_user 
-  ENCODING 'UTF8' 
-  LC_COLLATE = 'C' 
-  LC_CTYPE = 'C' 
+CREATE DATABASE synapse
+  OWNER synapse_user
+  ENCODING 'UTF8'
+  LC_COLLATE = 'C'
+  LC_CTYPE = 'C'
   TEMPLATE template0;
 GRANT ALL PRIVILEGES ON DATABASE synapse TO synapse_user;
