84a3f13c57f0ac869b93c23f8e451f4dcb70d416
Pierre 'McFly' Marty <pmarty@linagora.com>
2026-04-30 15:48:30 +0200

    Implements Dynamic CORS Origin Policy with Regex Support (#364)
    Signed-off-by: Pierre 'McFly' Marty <pmarty@linagora.com>

--- apps/tom-server/src/middleware/cors.ts
+++ apps/tom-server/src/middleware/cors.ts
@@ -14,16 +14,6 @@ import type { NextFunction, Request, RequestHandler, Response } from "express";
 
 import type { Config } from "../config/types";
 
-// type CorsOptions = {
-//   origin: string[] | ((origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => void);
-//   methods: string;
-//   allowedHeaders: string[];
-//   exposedHeaders?: string[];
-//   credentials: boolean;
-//   maxAge?: number;
-//   optionsSuccessStatus: number;
-// };
-
 /**
  * Creates CORS middleware configured from application settings.
  *
@@ -37,20 +27,38 @@ export function createCorsMiddleware(corsConfig: Config["cors"]): RequestHandler
     };
   }
 
+  let allow_all = false;
+  const exacts = new Set<string>();
+  const regexes: RegExp[] = [];
+
+  for (const rule of corsConfig.origins) {
+    if (rule === "*") {
+      allow_all = true;
+      break;
+    }
+
+    if (rule.startsWith("~")) {
+      regexes.push(new RegExp(rule.slice(1)));
+    } else {
+      exacts.add(rule);
+    }
+  }
+
   const options: CorsOptions = {
-    origin: corsConfig.origins,
+    // biome-ignore lint/nursery/useExplicitType: cors types does not export CustomOrigin type
+    origin: (o, cb) => {
+      if (allow_all) return cb(null, true);
+      if (!o && corsConfig.allow_no_origin) return cb(null, true);
+      if (o && (exacts.has(o) || regexes.some((rx) => rx.test(o)))) return cb(null, true);
+
+      cb(new Error(`${o} blocked by server CORS policy`));
+    },
     methods: corsConfig.methods,
     allowedHeaders: corsConfig.allowed_headers,
     credentials: corsConfig.credentials,
+    exposedHeaders: corsConfig.exposed_headers,
+    maxAge: corsConfig.max_age,
   };
-
-  if (corsConfig.exposed_headers !== undefined && corsConfig.exposed_headers.length > 0) {
-    options.exposedHeaders = corsConfig.exposed_headers;
-  }
-
-  if (corsConfig.max_age !== undefined) {
-    options.maxAge = corsConfig.max_age;
-  }
 
   return cors(options);
 }
